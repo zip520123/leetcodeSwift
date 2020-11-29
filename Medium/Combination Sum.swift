c2534088ae21017c0db022360ddb76a76c17bb92
zip520123 <zip520123@yahoo.com.tw>
2020-11-29 22:40:36 +0000

    add combination

---
+++ Medium/Combination Sum.swift
@@ -0,0 +1,24 @@
+/*Combination Sum*/
+func combinationSum(_ candidates: [Int], _ target: Int) -> [[Int]] {
+    var res = [[Int]]()
+    let count = candidates.count
+    func dfs(_ curr: Int,_ list: [Int],_ index: Int) {
+        if curr == 0 { 
+            res.append(list)
+            return
+        }
+        var list = list
+        for i in index..<count {
+            let n = candidates[i]
+            let rest = curr - n
+            if rest >= 0 {
+                list.append(n)
+                dfs(rest, list, i)
+                list.removeLast()
+            }
+        }
+    }
+    dfs(target, [], 0)
+    return res
+}
+
