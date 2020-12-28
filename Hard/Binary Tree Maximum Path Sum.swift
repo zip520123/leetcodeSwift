9786a534e644a289a154e927e40b0b633614d046
zip520123 <zip520123@yahoo.com.tw>
2020-12-28 18:32:14 +0000

    update binary tree maximum

--- Hard/Binary Tree Maximum Path Sum.swift
+++ Hard/Binary Tree Maximum Path Sum.swift
@@ -1,12 +1,12 @@
 /*Binary Tree Maximum Path Sum*/
 func maxPathSum(_ root: TreeNode?) -> Int {
     var res = -Int.max
-    func dfs(_ curr: TreeNode?) -> Int {
-        if curr == nil {return 0}
-        let leftValue = max(dfs(curr!.left),0)
-        let rightValue = max(dfs(curr!.right),0)
-        res = max(leftValue + rightValue + curr!.val, res)
-        return max(leftValue, rightValue) + curr!.val
+    func dfs(_ curr: TreeNode?) -> Int{
+        guard let curr = curr else {return 0}
+        let left = max(dfs(curr.left), 0)
+        let right = max(dfs(curr.right), 0)
+        res = max(left+right+curr.val, res)
+        return max(left, right) + curr.val
     }
     dfs(root)
     return res
