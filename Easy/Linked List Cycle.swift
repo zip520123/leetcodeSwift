e2a7a3481989f0405e288258f81ba715b1d85405
zip520123 <zip520123@yahoo.com.tw>
2020-12-31 22:41:35 +0000

    add Linked List cycle

---
+++ Easy/Linked List Cycle.swift
@@ -0,0 +1,15 @@
+/*Linked List Cycle*/
+//time O(n) space O(1)
+func hasCycle(_ head: ListNode?) -> Bool {
+    if head == nil {return false}
+    
+    var fast = head
+    var slow = head
+    while fast != nil && slow != nil {
+        if fast!.next == nil {return false}
+        slow = slow!.next
+        fast = fast!.next!.next
+        if fast === slow {return true}
+    }
+    return false
+}
