29098dfcfb24bd6d0f3793b5e3fc933991e78088
zip520123 <zip520123@yahoo.com.tw>
2021-01-07 12:59:15 +0000

    add reverse Lined list recursive

--- Easy/Reverse Linked List.swift
+++ Easy/Reverse Linked List.swift
@@ -1,4 +1,5 @@
 /*Reverse Linked List*/
+//time O(n) space O(1)
 func reverseList(_ head: ListNode?) -> ListNode? {
     var prev: ListNode? = nil
     var curr = head
@@ -10,4 +11,14 @@ func reverseList(_ head: ListNode?) -> ListNode? {
         curr = temp
     }
     return prev
+}
+//recursive time O(n) space O(1)
+func reverseList(_ head: ListNode?) -> ListNode? {
+    if head == nil || head!.next == nil{
+        return head
+    }
+    var node = reverseList(head!.next)
+    head!.next!.next = head
+    head!.next = nil
+    return node
 }
