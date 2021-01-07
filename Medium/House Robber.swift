a895c4f92c6c6c4b2f037a90352203fb93db2744
zip520123 <zip520123@yahoo.com.tw>
2021-01-07 12:20:04 +0000

    add space O(1) solution to house Robber

--- Medium/House Robber.swift
+++ Medium/House Robber.swift
@@ -1,4 +1,5 @@
 /*House Robber*/
+//time O(n) space O(n)
 func rob(_ nums: [Int]) -> Int {
     let ncount = nums.count
     if ncount == 0 {return 0}
@@ -15,4 +16,27 @@ func rob(_ nums: [Int]) -> Int {
         dp[i] = max(dp[i-2],dp[i-3]) + n
     }
     return max(dp[ncount - 1],dp[ncount - 2])
+}
+
+//time O(n) space O(1)
+func rob(_ nums: [Int]) -> Int {
+    let ncount = nums.count
+    if ncount == 0 {return 0}
+    if ncount == 1 {return nums[0]}
+    if ncount == 2 {return max(nums[0],nums[1])}
+    if ncount == 3 {return max(nums[0]+nums[2],nums[1])}
+    
+    var dp0 = nums[0]
+    var dp1 = nums[1]
+    var dp2 = nums[2] + nums[0]
+    
+    for i in 3..<ncount {
+        let n = nums[i]
+        
+        let temp = max(dp0,dp1) + n
+        dp0 = dp1
+        dp1 = dp2
+        dp2 = temp
+    }
+    return max(dp1,dp2)
 }
