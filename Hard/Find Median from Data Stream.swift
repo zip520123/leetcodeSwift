a931f1d79d0ccae64667e01ebbd927bb838acb5f
zip520123 <zip520123@yahoo.com.tw>
2021-02-03 12:45:10 +0000

    Add another method to Find Median

--- Hard/Find Median from Data Stream.swift
+++ Hard/Find Median from Data Stream.swift
@@ -5,7 +5,6 @@ class MedianFinder {
     
     func addNum(_ num: Int) {
         //O (log n) insert sort
-        
         var left = 0, right = arr.endIndex
         while left < right {
             let mid = left + (right - left) / 2
@@ -13,8 +12,7 @@ class MedianFinder {
                 left = mid + 1
             } else {
                 right = mid
-            }
-            
+            }   
         }
         arr.insert(num, at: left)
     }
@@ -27,6 +25,35 @@ class MedianFinder {
             let n = arr[arr.endIndex / 2]
             let n2 = arr[arr.endIndex / 2 - 1]
             return (Double(n) + Double(n2)) / 2
+        }
+    }
+}
+
+class MedianFinder {
+    var arr = [Int]()
+    
+    func addNum(_ num: Int) {
+        var left = 0, right = arr.endIndex
+        
+        while left < right {
+            let mid = left + (right - left) >> 1
+            if arr[mid] < num {
+                left = mid + 1
+            } else {
+                right = mid
+            }
+            
+        }
+        arr.insert(num, at: left)
+    }
+    
+    func findMedian() -> Double {
+        if arr.endIndex % 2 == 1 {
+            return Double(arr[arr.endIndex >> 1])
+        } else {
+            let n1 = Double(arr[arr.endIndex >> 1])
+            let n2 = Double(arr[arr.endIndex >> 1 - 1])
+            return n1 + (n2 - n1) / 2
         }
     }
 }
