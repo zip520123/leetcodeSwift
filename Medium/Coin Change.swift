b0391230be388d38b91b6b0ab91911d3a246db2c
zip520123 <zip520123@yahoo.com.tw>
2021-02-04 19:31:22 +0000

    Add coin change, longest increasing subsequence

---
+++ Medium/Coin Change.swift
@@ -0,0 +1,70 @@
+/*Coin Change*/
+func coinChange(_ coins: [Int], _ amount: Int) -> Int {
+    //time O(amount ^ coins) space O(n) TLE
+    if amount == 0 {return 0}
+    var res = Int.max
+    func dfs(_ currAmount: Int, _ count: Int) {
+        if currAmount == 0 {
+            res = min(res, count)
+            return
+        }
+        for n in coins {
+            if currAmount - n >= 0 {
+                dfs(currAmount - n, count + 1)
+            }
+        }
+    }
+    dfs(amount, 0)
+    if res == Int.max {
+        return -1
+    }
+    return res
+}
+
+//f(0) = 0
+//f(s) = -1 when no coin available
+//f(s) = min(each_coin( f(s - c) + 1 )
+func coinChange(_ coins: [Int], _ amount: Int) -> Int {
+    var arr = Array<Int>(repeating: 0, count: amount + 1)
+    //time O(amount * coins) space O(amount)
+    func dfs(_ rest: Int) -> Int {
+        if rest == 0 {return 0}
+        if arr[rest] != 0 {return arr[rest]}
+        var min = Int.max
+        for coin in coins {
+            let val = rest - coin
+            if val >= 0 {
+                let res = dfs(val)
+                if res != -1 {
+                    min = Swift.min(res + 1, min)
+                }
+            }
+        }
+        min = min == Int.max ? -1 : min
+        arr[rest] = min
+        return min
+    }
+    return dfs(amount)
+}
+
+func coinChange(_ coins: [Int], _ amount: Int) -> Int {
+    if amount < 1 {return 0}
+    var arr = Array<Int>(repeating: 0, count: amount)
+    
+    func dfs(_ rest: Int) -> Int {
+        if rest < 0 {return -1}
+        if rest == 0 {return 0}
+        if arr[rest - 1] != 0 {return arr[rest - 1]}
+        var min = Int.max
+        for coin in coins {
+            let res = dfs(rest - coin)
+            if res >= 0 && res < min {
+                min = res + 1
+            }
+        }
+        arr[rest - 1] = min == Int.max ? -1 : min
+        return arr[rest - 1]
+    } 
+    
+    return dfs(amount)
+}
---
+++ Medium/Longest Increasing Subsequence.swift
@@ -0,0 +1,44 @@
+/*Longest Increasing Subsequence*/
+//time O(n^2) space O(n)
+func lengthOfLIS(_ nums: [Int]) -> Int {
+    var dp = Array<Int>(repeating:1, count: nums.endIndex)
+    var max = 1
+    var i = 1
+    while i < nums.endIndex {
+        let n = nums[i]
+        var j = 0
+        while j < i {
+            let n2 = nums[j]
+            if n2 < n {
+                dp[i] = Swift.max(dp[i], dp[j] + 1)
+                max = Swift.max(max, dp[i])
+            }
+            j += 1
+        }
+        i += 1
+    }
+    return max
+}
+
+//time O(n log n) space O(n)
+func lengthOfLIS(_ nums: [Int]) -> Int {
+    var dp = [Int]()
+    for n in nums {
+        var left = 0, right = dp.endIndex
+        while left < right {
+            let mid = left + (right - left) >> 1
+            if dp[mid] < n {
+                left = mid + 1
+            } else {
+                right = mid
+            }
+        }
+        if left == dp.endIndex {
+            dp.append(n)
+        } else {
+            dp[left] = n
+        }
+        
+    }
+    return dp.endIndex
+}
