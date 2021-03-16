3ea2404bce92b2f0898cb5bb09ab727f657db2cb
zip520123 <zip520123@yahoo.com.tw>
2021-03-16 15:48:38 +0000

    update search in Rotated Sorted Array

--- Medium/Search in Rotated Sorted Array.swift
+++ Medium/Search in Rotated Sorted Array.swift
@@ -1,48 +1,29 @@
 /*Search in Rotated Sorted Array*/
+//O(log n)
 func search(_ nums: [Int], _ target: Int) -> Int {
-    let ncount = nums.count
-    var left = 0, right = ncount - 1
-    var start = 0
-    while(left < right) {
-        let mid = left + (right - left) / 2
-        if mid - 1 >= 0 && nums[mid] < nums[mid - 1] {
-            start = mid 
-        }
-        if mid + 1 < ncount && nums[mid] > nums[mid + 1] {
-            start = mid + 1
-        }
-        if nums[mid] > nums[left] {
-            left = mid + 1
-        } else {
-            right = mid - 1
-        }
-    }
-
-    if nums[start] == target {
-        return start
-    } 
-    left = 0
-    right = ncount - 1
-    
-    if start != 0 && target >= nums[left]{ 
-        right = start
-    } else {
-        left = start
-    }
-    
-    while(left < right) {
-        let mid = left + (right - left) / 2
-        if nums[mid] < target {
-            left = mid + 1
-        } else if nums[mid] > target {
-            right = mid - 1
-        } else {
+    var left = 0, right = nums.endIndex - 1
+    while left <= right {
+        let mid = (left + right ) >> 1
+        if target == nums[mid] {
             return mid
         }
-    }
-    
-    if nums[left] == target {return left}
+        //left sorted postion 
+        if nums[left] <= nums[mid] {
+            if target > nums[mid] || target < nums[left] {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        } else {
+            //right postion
+            if target < nums[mid] || target > nums[right] {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
 
+    }
     return -1
 }
 
