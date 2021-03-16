f812e426c062c33b39a5ecbf4bd87be88876ffcf
zip520123 <zip520123@yahoo.com.tw>
2021-03-16 22:53:55 +0000

    Introduce insert interal new solution

--- Medium/Insert Interval.swift
+++ Medium/Insert Interval.swift
@@ -45,6 +45,33 @@ func insert(_ intervals: [[Int]], _ newInterval: [Int]) -> [[Int]] {
     return res
 }
 
+//time O(n log n) space O(n)
+func insert(_ intervals: [[Int]], _ newInterval: [Int]) -> [[Int]] {
+    let inter = (intervals + [newInterval]).sorted(by:{ 
+        a,b in
+        if a[0] != b[0] {
+            return a[0] < b[0]
+        } else {
+            return a[1] < b[1]
+        }
+    })
+
+    var res = [[Int]]()
+    var curr = inter[0]
+    
+    for i in 1..<inter.endIndex {
+        let next = inter[i]
+        if curr[1] >= next[0] {
+            curr[1] = max(curr[1],next[1])
+        }else {
+            res.append(curr)
+            curr = next
+        }
+    }
+    res.append(curr)
+    
+    return res
+}
 let input = [[1,3],[6,9]]
 let input2 = [2,5]
 print(insert(input, input2))
