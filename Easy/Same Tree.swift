5b480b88cb20587451f7107efc2c5a6ce4ceede4
zip520123 <zip520123@yahoo.com.tw>
2021-03-17 20:02:45 +0000

    update same tree

--- Easy/Same Tree.swift
+++ Easy/Same Tree.swift
@@ -2,38 +2,29 @@
 //time O(n) space O(log(N)) in the best case of completely balanced tree O(n) in the worst case of completely unbalanced tree, to keep a recursion stack.
 
 func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
-    if p == nil && q != nil || p != nil && q == nil {return false}
     if p == nil && q == nil {return true}
-    
-    let res = isSameTree(p!.left, q!.left) && isSameTree(p!.right,q!.right)
-    return res && p!.val == q!.val
+    if p == nil || q == nil {return false}
+    if p!.val != q!.val {return false}
+    return isSameTree(p!.left,q!.left) && isSameTree(p!.right,q!.right)
 }
 
 //time O(n) space O(n)
 func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+    var stack1 = [p]
+    var stack2 = [q]
     
-    if p == nil && q != nil || p != nil && q == nil {return false}
-    if p == nil && q == nil {return true}
-    
-    var leftStack = [TreeNode?]()
-    var rightStack = [TreeNode?]()
-    leftStack.append(p)
-    rightStack.append(q)
-    
-    while(leftStack.isEmpty == false || rightStack.isEmpty == false) {
-        let oleft = leftStack.removeFirst()
-        let oright = rightStack.removeFirst()
+    while !stack1.isEmpty {
+        let left = stack1.removeFirst()
+        let right = stack2.removeFirst()
+        if left == nil && right == nil {continue}
+        if left == nil || right == nil {return false}
+        guard let leftNode = left, let rightNode = right else {return false}
+        if leftNode.val != rightNode.val {return false}
+        stack1.append(leftNode.left)
+        stack2.append(rightNode.left)
+        stack1.append(leftNode.right)
+        stack2.append(rightNode.right)
         
-        if oleft == nil && oright != nil || oleft != nil && oright == nil {return false}
-        guard let left = oleft, let right = oright else { continue }
-        if left.val != right.val {
-            return false
-        }    
-        leftStack.append(left.left)
-        rightStack.append(right.left)
-        leftStack.append(left.right)
-        rightStack.append(right.right)
-    
     }
     return true
 }
