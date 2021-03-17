734b9ceac8122187d9d8ab339e591c66d6dfb26f
zip520123 <zip520123@yahoo.com.tw>
2021-03-17 19:16:49 +0000

    update Decode ways

--- Medium/Decode Ways.swift
+++ Medium/Decode Ways.swift
@@ -1,27 +1,48 @@
 /*Decode Ways*/
 func numDecodings(_ s: String) -> Int {
-    let sArr = Array(s)
-    let scount = s.count
-    var dp = Array<Int>(repeating: 0, count: scount + 1)
+    var dp = Array<Int>(repeating: 0, count: s.count + 1)
     dp[0] = 1
-    if Int(String(sArr[0]))! != 0 {
-        dp[1] = 1
-    }
-    if scount < 2 {
-        return dp[scount]
-    }
-    for i in 2...scount {
-        if let one = Int(String(sArr[i-1])) {
-            if one >= 1 {
-                dp[i] += dp[i-1]
-            }
+    let sArr = Array(s)
+    
+    for i in 1...sArr.endIndex {
+        if Int(String(sArr[i - 1]))! > 0 {
+            dp[i] += dp[i - 1]
         }
-        if let two = Int(String(sArr[i-2..<i])) {
-            if two <= 26 && two >= 10 {
-                dp[i] += dp[i-2]
-            }
+        if i >= 2 {
+            let two = Int(String(sArr[i-2...i-1]))!
+            if two >= 10 && two <= 26 {
+                dp[i] += dp[i - 2]
+            }    
+        }
+    }
+    return dp[sArr.endIndex]
+}
+
+//time O(n) space O(n)
+func numDecodings(_ s: String) -> Int {
+    var dict = Dictionary<String,Int>()
+    
+    func decode(_ s: String) -> Int {
+        if dict[s] != nil {
+            return dict[s]!
+        }
+        let sArr = Array(s)
+        if sArr.endIndex == 0 {return 1}
+        
+        var res = 0
+        
+        if Int(String(sArr[0]))! > 0 {
+            res += decode(String(sArr[1..<sArr.endIndex]))
+        } 
+        if sArr.endIndex >= 2 {
+            if Int(String(sArr[0...1]))! >= 10 && Int(String(sArr[0...1]))! <= 26 {
+                res += decode(String(sArr[2..<sArr.endIndex]))
+            }    
         }
         
+        dict[s] = res
+        return res
     }
-    return dp[scount]
+    
+    return decode(s)
 }
