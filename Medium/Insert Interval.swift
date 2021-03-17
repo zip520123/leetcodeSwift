83decf73774c41f4d7d2db220a387cb6889a7998
zip520123 <zip520123@yahoo.com.tw>
2021-03-17 13:41:34 +0000

    Update insert interval

--- Medium/Insert Interval.swift
+++ Medium/Insert Interval.swift
@@ -1,49 +1,42 @@
 /*Insert Interval*/
 //time O(n) space O(n)
 func insert(_ intervals: [[Int]], _ newInterval: [Int]) -> [[Int]] {
+        if intervals.endIndex == 0 {return [newInterval]}
+        if newInterval[1] < intervals[0][0] {
+            return [newInterval] + intervals
+        }
+        if newInterval[0] > intervals.last![1] {
+            return intervals + [newInterval]
+        }
         
-    let icount = intervals.count
-    if icount == 0 {
-        return [newInterval]
-    }
-    var res = [[Int]]()
-    if newInterval[1] < intervals[0][0] {
-        res.append(newInterval)
-        res += intervals
+        var res = [[Int]]()
+
+        for (i,item) in intervals.enumerated() {
+            if newInterval[0] <= item[1] {
+                if newInterval[1] < item[0] {
+                    res.append(newInterval)
+                    res.append(item)
+                } else {
+                    res.append([min(item[0],newInterval[0]),max(item[1],newInterval[1])])    
+                }
+                for j in i+1..<intervals.endIndex {
+                    let curr = intervals[j]
+                    let prev = res.last!
+                    if curr[0] <= prev[1] {
+                        res.removeLast()
+                        res.append([prev[0],max(curr[1],prev[1])])
+                    } else {
+                        res.append(curr)
+                    }
+                }
+                break
+            } else {
+                res.append(item)
+            }
+        }
+        
         return res
     }
-    
-    for (i, item) in intervals.enumerated() {
-        if item[1] >= newInterval[0] {
-            if item[0] > newInterval[1] {
-                res.append(newInterval)
-                res.append(item)
-            } else {
-                res.append([min(item[0],newInterval[0]),max(item[1],newInterval[1])])    
-            }
-            
-            for j in i..<icount {
-                let prev = res.last!
-                let curr = intervals[j]
-                if prev[1] >= curr[0] {
-                    res.removeLast()
-                    res.append([prev[0], max(curr[1],prev[1])])
-                } else {
-                    res.append(curr)
-                }
-            }
-            break
-        } else {
-            res.append(item)
-        }
-    }
-    
-    if res.last![1] < newInterval[0] {
-        res.append(newInterval)
-    }
-    
-    return res
-}
 
 //time O(n log n) space O(n)
 func insert(_ intervals: [[Int]], _ newInterval: [Int]) -> [[Int]] {
