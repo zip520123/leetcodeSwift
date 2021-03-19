f8dcdc923dbeda8c32a9ca5993ef4c13a7bf5151
zip520123 <zip520123@yahoo.com.tw>
2021-03-19 21:16:45 +0000

    Added Trie tree solution to word search

--- Hard/Word Search II.swift
+++ Hard/Word Search II.swift
@@ -1,5 +1,5 @@
 /*Word Search II*/
-//dfs time O(words * row * col) space O(1) or O(board)
+//dfs time O(words * row * col) space O(1) or O(board) TLE
 func findWords(_ board: [[Character]], _ words: [String]) -> [String] {
     var board = board
     var res = [String]()
@@ -43,7 +43,7 @@ func findWords(_ board: [[Character]], _ words: [String]) -> [String] {
     }
     return res
 }
-//dfs + map time O(board + words) space O(board)
+//dfs + map time O(board + words) space O(board) TLE
 func findWords(_ board: [[Character]], _ words: [String]) -> [String] {
     var board = board
     var res = [String]()
@@ -94,5 +94,76 @@ func findWords(_ board: [[Character]], _ words: [String]) -> [String] {
         }
     }
         
+    return res
+}
+
+//trie tree 
+class Trie {
+    var dict = Dictionary<Character, Trie>()
+    var word: String?
+}
+func findWords(_ board: [[Character]], _ words: [String]) -> [String] {
+    let root = Trie()
+    
+    for word in words {
+        var curr = root 
+        for char in word {
+            if curr.dict[char] == nil {
+                let newNode = Trie()
+                curr.dict[char] = newNode
+                curr = newNode
+            } else {
+                curr = curr.dict[char]!
+            }
+        }
+        curr.word = word
+    }
+    
+    var board = board 
+    let row = board.endIndex
+    let col = board[0].endIndex
+    var res = [String]()
+    
+    func dfs(_ x: Int, _ y: Int, _ node: Trie) {
+        if x < 0 || y < 0 || x == row || y == col { return }
+        guard let next = node.dict[board[x][y]] else {return}
+        if let word = next.word {
+            res.append(word)
+            next.word = nil
+        }
+        let temp = board[x][y]
+        
+        board[x][y] = "_"
+        dfs(x+1,y,next)
+        dfs(x-1,y,next)
+        dfs(x,y+1,next)
+        dfs(x,y-1,next)
+        board[x][y] = temp
+
+                // SPEED UP TRICK:
+        // What are we doing here?
+        // While backtracking we removing all the leaf nodes and as we go up we would be also be removing some of
+        // parent nodes that now became leaf nodes because of the removal of the children. By doing this we are making the 
+        // Trie much smaller 
+        
+        // Now we know what we are doing here
+        // Lets understand why we are doing this
+        // We reach a leaf node only if we found a string matching that leaf node on the board
+        // All leaf nodes would be the end of a complete word, so after we find the word there is no need to 
+        // keep the elements of the word as we dont need duplicates. Finding the word once is enough. Incase we also wanted
+        // how many times a particular word occured in the board we should not do this but in this case we dont need that. So we can safely remove the leaf node and we would also be removing its parent while we backtrack till the beginning of the word until we see that the parent has atleast one child.
+        
+        // This severely speeds up the algorithm 2400ms -> 4ms!!!!!!
+        if next.dict.isEmpty {
+            node.dict[temp] = nil
+        }
+    } 
+    
+    for i in 0..<row {
+        for j in 0..<col {
+            dfs(i,j,root)
+        }
+    }
+
     return res
 }
