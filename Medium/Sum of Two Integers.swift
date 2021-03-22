a1f26a3f99ae5c37b7fa6868547331354d157228
zip520123 <zip520123@yahoo.com.tw>
2021-03-22 20:49:50 +0000

    Introduced new solution to Sum of the

--- Medium/Sum of Two Integers.swift
+++ Medium/Sum of Two Integers.swift
@@ -2,3 +2,17 @@ func getSum(_ a: Int, _ b: Int) -> Int {
     return a + b
 }
 
+func getSum(_ a: Int, _ b: Int) -> Int {
+    var a = a
+    var b = b
+    while b != 0 {
+        let carry = a & b
+        a = a ^ b
+        b = carry << 1
+    }
+    return a
+}
+
+func getSum_Recursive(_ a: Int, _ b: Int) -> Int {
+    return b == 0 ? a : getSum_Recursive(a ^ b, (a & b) << 1)
+}
