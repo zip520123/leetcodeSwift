07a7da49baaae73e63b4e8ed6eb9d1c2944a2c0d
zip520123 <zip520123@yahoo.com.tw>
2021-03-22 20:18:34 +0000

    Introduced new solution to LengthOfLIS

--- Medium/Longest Increasing Subsequence.swift
+++ Medium/Longest Increasing Subsequence.swift
@@ -42,3 +42,18 @@ func lengthOfLIS(_ nums: [Int]) -> Int {
     }
     return dp.endIndex
 }
+//time O(n^2) space O(n)
+func lengthOfLIS(_ nums: [Int]) -> Int {
+    var dp = [Int](repeating: 1, count: nums.endIndex)
+    var res = 1
+    for i in 0..<nums.endIndex {
+        for j in 0..<i {
+            if nums[j] < nums[i] {
+                dp[i] = max(dp[i], dp[j] + 1)
+                res = max(res, dp[i])
+            }
+        }
+    }
+    
+    return res
+}
