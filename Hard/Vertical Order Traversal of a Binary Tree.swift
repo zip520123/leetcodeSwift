3b6af16a0da024e235baf517295c3dcd46d5cd31
zip520123 <zip520123@yahoo.com.tw>
2021-05-12 20:24:05 +0100

    Vertical order Traversal

--- Hard/Vertical Order Traversal of a Binary Tree.swift
+++ Hard/Vertical Order Traversal of a Binary Tree.swift
@@ -22,7 +22,7 @@ class Solution {
     }
 }
 
-        func verticalTraversal(_ root: TreeNode?) -> [[Int]] {
+    func verticalTraversal(_ root: TreeNode?) -> [[Int]] {
         var dict = [Int:[Int:[Int]]]()
         
         func dfs(_ node:TreeNode?, _ col: Int, _ row: Int) {
@@ -40,5 +40,23 @@ class Solution {
                     .flatMap {$0.value.sorted()}
             }
             
+        return list
+    }
+
+    func verticalTraversal(_ root: TreeNode?) -> [[Int]] {
+        
+        var dict = [Int:[Int:[Int]]]()
+        
+        func dfs(_ row: Int, _ col: Int, _ node: TreeNode?) {
+            guard let node = node else {return}
+            dict[col, default:[:]][row, default:[]].append(node.val)
+            dfs(row+1, col-1, node.left)
+            dfs(row+1, col+1, node.right)
+        }
+        dfs(0,0, root)
+        let list = dict.sorted { $0.key < $1.key }.map { $0.value }
+                    .map { $0.sorted { $0.key < $1.key }
+                           .flatMap {$0.value.sorted()}
+                         }
         return list
     }
