a26a3f1e2e1bcbbef8cb8116ca0f39546e0e0502
zip520123 <zip520123@yahoo.com.tw>
2021-05-12 22:18:49 +0100

    word break

--- Medium/Word Break.swift
+++ Medium/Word Break.swift
@@ -1,108 +1,20 @@
 /*Word Break*/
-//time O(s^wordDict) space O(wordDict)
-func wordBreak(_ s: String, _ wordDict: [String]) -> Bool {
-    let set = Set(wordDict)
-    func dfs(_ s: String) -> Bool {
-        let arrS = Array(s)
-        let scount = arrS.count
-        if scount == 0 {
-            return true
-        }
-        var res = false
-        for word in set {
-            var wordIndex = 0
-            
-            var wcount = word.count
-            var arrW = Array(word)
-            
-            while wordIndex < wcount && wordIndex < scount {
-                if arrW[wordIndex] != arrS[wordIndex] {
-                    break
-                }
-                wordIndex += 1
-            }
-            if wordIndex == wcount {
-                res = res || dfs(String(arrS[wcount..<scount])) 
-            }
-        }
-        return res
-    }
-    return dfs(s)
-}
-//dp time O(n) space O(n)
-func wordBreak(_ s: String, _ wordDict: [String]) -> Bool {
-    var dict = [String:Bool]()
-    func dfs(_ s: String) -> Bool {
-        if let res = dict[s] {
-            return res
-        }
-        let scount = s.count
-        if scount == 0 {
-            return true
-        }
-        let arrS = Array(s)
-        
-        var res = false
-        for word in wordDict {
-            let arrW = Array(word)
-            let wcount = arrW.count
-            var i = 0
-            while (i < wcount && i < scount) {
-                if arrW[i] != arrS[i] {
-                    break
-                }
-                i += 1
-            }
-            if i == wcount {
-                res = res || dfs(String(arrS[wcount..<scount]))
-            }
-        }
-        dict[s] = res
-        return res
-    }
-    return dfs(s)
-}
-
-func wordBreak(_ s: String, _ wordDict: [String]) -> Bool {
-    var dict = Dictionary<String, Bool>()
-    
-    func dfs(_ s:String) -> Bool {
-        if s.count == 0 {return true}
-        if dict[s] != nil {
-            return dict[s]!
-        }
-        var res = false
-        for word in wordDict {
-            if s.hasPrefix(word) {
-                let sArr = Array(s)
-                let rest = Array(sArr[word.count..<sArr.endIndex]).map{String($0)}.joined()
-                res = res || dfs(rest)
-            }
-        }
-        dict[s] = res
-        return res
-    }
-    
-    return dfs(s)
-}
 //O(s.length^3) O(s.length)
 func wordBreak(_ s: String, _ wordDict: [String]) -> Bool {
     var dict = [String:Bool]()
     func dfs(_ s: String) -> Bool {
+        if dict[s] != nil {return dict[s]!}
         if s == "" {return true}
-        if dict[s] != nil {
-            return dict[s]!
-        }
         let sArr = Array(s)
         var res = false
         for word in wordDict {
             let wArr = Array(word)
             var i = 0
-            while i < sArr.endIndex && i < wArr.endIndex && sArr[i] == wArr[i] {
+            while i < sArr.endIndex && i < wArr.endIndex && sArr[i] == wArr[i]{
                 i += 1
             }
             if i == wArr.endIndex {
-                res = res || dfs(String(sArr[i..<sArr.endIndex]))
+                res = res || dfs(String(sArr[i...]))
             }
         }
         dict[s] = res
