024a7abb71f5cbbbc8a7ecf4fc3c3cff02424d01
zip520123 <zip520123@yahoo.com.tw>
2021-06-07 13:22:34 +0100

    Min Cost Climbing Stairs

--- Easy/Min Cost Climbing Stairs.swift
+++ Easy/Min Cost Climbing Stairs.swift
@@ -7,3 +7,13 @@ func minCostClimbingStairs(_ cost: [Int]) -> Int {
     }
     return dp[dp.endIndex - 1]
 }
+
+func minCostClimbingStairs(_ cost: [Int]) -> Int {
+    var dp = (0...cost.endIndex).map {_ in 0}
+    var i = 2
+    while i < dp.endIndex {
+        dp[i] = min(dp[i-2]+cost[i-2],dp[i-1]+cost[i-1])
+        i += 1
+    }
+    return dp.last!
+}
