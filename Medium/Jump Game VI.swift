87e54f1e3feae41925f73ba012e8f3cb7409262a
zip520123 <zip520123@yahoo.com.tw>
2021-06-09 11:24:56 +0100

    Jump game VI

---
+++ Medium/Jump Game VI.swift
@@ -0,0 +1,20 @@
+//Jump Game VI
+//O(n), O(n)
+    func maxResult(_ nums: [Int], _ k: Int) -> Int {
+        var dp = (0..<nums.endIndex).map {_ in 0}
+        var queue = [0]
+        dp[0] = nums[0]
+        for i in 1..<nums.endIndex {
+            let n = nums[i]
+            let curr = dp[queue.first!] + n
+            dp[i] = curr
+            while !queue.isEmpty && dp[queue.last!] < curr {
+                queue.removeLast()
+            }
+            queue.append(i)
+            if queue.first! == i-k {
+                queue.removeFirst()
+            }
+        }
+        return dp.last!
+    }
