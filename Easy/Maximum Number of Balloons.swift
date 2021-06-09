1564a191f4ea71c31742d520eb954b7f6a21c5c3
zip520123 <zip520123@yahoo.com.tw>
2021-06-09 11:25:14 +0100

    Maximum number of balloons

---
+++ Easy/Maximum Number of Balloons.swift
@@ -0,0 +1,34 @@
+//Maximum Number of Balloons
+//O(n), O(n)
+    func maxNumberOfBalloons(_ text: String) -> Int {
+        var res = Int.max
+        var dict = [Character: Int]()
+        for char in text {
+            dict[char, default:0] += 1
+        }
+        for (char, value) in ["n":1,"l":2,"a":1,"b":1,"o":2] {
+            let currCount = dict[Character(char), default:0]
+            res = min(res, currCount / value)
+        }
+        return res
+    }
+
+    	// O(n) time | O(1) space
+    func maxNumberOfBalloons(_ text: String) -> Int {
+        var b = 0, a = 0, l = 0, o = 0, n = 0
+
+        for char in text {
+            switch char {
+            case "b": b += 1
+            case "a": a += 1
+            case "l": l += 1
+            case "o": o += 1
+            case "n": n += 1
+            default: continue
+            }
+        }
+
+        l /= 2; o /= 2 // we need 2 letters of each
+
+        return min(b, a, l, o, n)
+    }
