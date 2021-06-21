6b8efe434baf795cd09b196a97cff83b27941b2a
zip520123 <zip520123@yahoo.com.tw>
2021-06-21 09:24:13 +0100

    Pascal's Triangle

--- Easy/Pascal's Triangle.swift
+++ Easy/Pascal's Triangle.swift
@@ -16,3 +16,17 @@
         }
         return res
     }
+
+    func generate(_ numRows: Int) -> [[Int]] {
+        var res = (0..<numRows).map { n in (0...n).map{_ in 1} }
+        
+        for i in 1..<numRows {
+            var n = 1 
+            while n < i {
+                res[i][n] = res[i-1][n] + res[i-1][n-1]    
+                n += 1
+            }
+            
+        }
+        return res
+    }
