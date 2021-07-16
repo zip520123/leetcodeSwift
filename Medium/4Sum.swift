036f3e2403199c9f8685e6459402ee76e6cebb41
zip520123 <zip520123@yahoo.com.tw>
2021-07-16 09:18:20 +0100

    4sum

--- Medium/4Sum.swift
+++ Medium/4Sum.swift
@@ -49,3 +49,35 @@ func fourSum(_ nums: [Int], _ target: Int) -> [[Int]] {
         }
         return res
     }
+
+
+    func fourSum(_ nums: [Int], _ target: Int) -> [[Int]] {
+        if nums.endIndex < 4 {return []}
+        var res = [[Int]]()
+        let nums = nums.sorted()
+        for i in 0..<nums.endIndex-3 {
+            if i>0 && nums[i-1] == nums[i] {continue}
+            for j in i+1..<nums.endIndex-2 {
+                if j > i+1 && nums[j] == nums[j-1] {continue}
+                var k = j+1, l = nums.endIndex-1 
+                while k<l {
+                    let n = nums[i] + nums[j] + nums[k] + nums[l]
+                    if n == target {
+                        res.append([nums[i], nums[j], nums[k], nums[l]])
+                        while k+1<l && nums[k] == nums[k+1] { k+=1 }
+                        k += 1
+                        while l-1>k && nums[l] == nums[l-1] {l-=1}
+                        l-=1
+                    } else if n < target {
+                        while k+1<l && nums[k] == nums[k+1] { k+=1 }
+                        k+=1
+                    } else if n > target {
+                        while l-1>k && nums[l] == nums[l-1] {l-=1}
+                        l-=1
+                    }   
+                }
+                
+            }
+        }
+        return res
+    }
