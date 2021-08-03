4ccf13f7d069264f7ab8683afaab275a59466773
zip520123 <zip520123@yahoo.com.tw>
2021-08-03 14:13:27 +0100

    Subsets II

--- Medium/Subsets II.swift
+++ Medium/Subsets II.swift
@@ -17,3 +17,20 @@
         dfs([],0)
         return res
     }
+
+    func subsetsWithDup(_ nums: [Int]) -> [[Int]] {
+        var res = [[Int]]()
+        let nums = nums.sorted()
+        func dfs(_ path:[Int], _ index: Int) {
+            res.append(path)
+            var prev: Int?
+            for i in index..<nums.endIndex {
+                let curr = nums[i]
+                if prev != nil && curr == prev! {continue}
+                prev = curr
+                dfs(path+[curr], i+1)
+            }
+        }
+        dfs([], 0)
+        return res
+    }
