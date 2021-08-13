618743ef519501f8cc42597b7e77aa59502e6009
zip520123 <zip520123@yahoo.com.tw>
2021-08-13 10:53:43 +0100

    Set Matrix Zeroes

--- Medium/Set Matrix Zeroes.swift
+++ Medium/Set Matrix Zeroes.swift
@@ -76,3 +76,78 @@ func setZeroes(_ matrix: inout [[Int]]) {
         i+=1
     }
 }
+
+//O(n*m), O(n+m)
+    func setZeroes(_ matrix: inout [[Int]]) {
+        let rows = matrix.endIndex, cols = matrix[0].endIndex
+        var rowsMark = Set<Int>(), colsMark = Set<Int>()
+        for i in 0..<rows {
+            for j in 0..<cols {
+                if matrix[i][j] == 0 {
+                    rowsMark.insert(i)
+                    colsMark.insert(j)
+                }
+            }
+        }
+
+        for i in rowsMark {
+            for j in 0..<cols {
+                matrix[i][j] = 0
+            }
+        }
+        
+        for i in 0..<rows {
+            for j in colsMark {
+                matrix[i][j] = 0
+            }    
+        }
+        
+    }
+
+//O(n*m), O(1)
+    func setZeroes(_ matrix: inout [[Int]]) {
+        var firstRow = false, firstCol = false
+        let rows = matrix.endIndex, cols = matrix[0].endIndex
+        for i in 0..<cols {
+            if matrix[0][i] == 0 {
+                firstRow = true
+            }
+        }
+        for i in 0..<rows {
+            if matrix[i][0] == 0 {
+                firstCol = true
+            }
+        }
+        for i in 1..<rows {
+            for j in 1..<cols {
+                if matrix[i][j] == 0 {
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+                }
+            }
+        }
+        for i in 1..<cols {
+            if matrix[0][i] == 0 {
+                for j in 0..<rows {
+                    matrix[j][i] = 0
+                }
+            }
+        }
+        for i in 1..<rows {
+            if matrix[i][0] == 0 {
+                for j in 0..<cols {
+                    matrix[i][j] = 0
+                }
+            }
+        }
+        if firstRow {
+            for i in 0..<cols {
+                matrix[0][i] = 0
+            }
+        }
+        if firstCol {
+            for i in 0..<rows {
+                matrix[i][0] = 0
+            }
+        }
+    }
