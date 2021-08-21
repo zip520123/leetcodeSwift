b6135edfb388cf7470ceae2d8d4cf21afa59c95a
zip520123 <zip520123@yahoo.com.tw>
2021-08-21 10:34:13 +0100

    Sudoku Solver

--- Hard/Sudoku Solver.swift
+++ Hard/Sudoku Solver.swift
@@ -35,7 +35,41 @@
             }
             return true
         }
-        
         dfs()
-        
     }
+
+class Solution {
+    func solveSudoku(_ board: inout [[Character]]) {
+        func dfs() -> Bool {
+            for i in 0..<9 {
+                for j in 0..<9 {
+                    if board[i][j] == "." {
+                        for k in 1...9 {
+                            let char = Character(String(k))
+                            if check(board, i, j, char) == true {
+                                board[i][j] = char
+                                if dfs() == true {
+                                    return true
+                                }
+                            } 
+                            board[i][j] = "." 
+                        }
+                        return false
+                    }
+                }
+            }
+            return true
+        }
+        dfs()
+    }
+    
+    func check(_ board: [[Character]], _ row: Int, _ col: Int, _ char: Character) -> Bool {
+        var count = 0
+        for i in 0..<9 {
+            if board[row][i] == char {count+=1}
+            if board[i][col] == char {count+=1}
+            if board[(row/3)*3+i/3][(col/3)*3+i%3] == char {count+=1}
+        }
+        return count == 0
+    }
+}
