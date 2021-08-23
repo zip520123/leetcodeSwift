9b693bb34400e06069a22a1a96dc782ed4139c15
zip520123 <zip520123@yahoo.com.tw>
2021-08-23 18:45:11 +0100

    Two Sum IV - Input is a BST

---
+++ Easy/Two Sum IV - Input is a BST.swift
@@ -0,0 +1,15 @@
+//Two Sum IV - Input is a BST
+//O(n), O(n)
+    func findTarget(_ root: TreeNode?, _ k: Int) -> Bool {
+        var set = Set<Int>()
+        func dfs(_ node: TreeNode?) -> Bool {
+            guard let node = node else {return false}
+            if set.contains(k-node.val) {return true}
+            set.insert(node.val)
+            if dfs(node.left) || dfs(node.right) {
+                return true
+            } 
+            return false
+        }
+        return dfs(root)
+    }
