0d5105ed4581fcf576551b0df9c2f31a91da707c
zip520123 <zip520123@yahoo.com.tw>
2021-09-02 09:59:25 +0100

    Unique Binary Search Trees II

--- Medium/Unique Binary Search Trees II.swift
+++ Medium/Unique Binary Search Trees II.swift
@@ -26,3 +26,27 @@ class Solution {
         return res
     } 
 }
+
+class Solution {
+    func generateTrees(_ n: Int) -> [TreeNode?] {
+        return bst(1,n)
+    }
+    
+    func bst(_ l: Int, _ r: Int) -> [TreeNode?] {
+        if l > r {return [nil]}
+        var res = [TreeNode?]()
+        for i in l...r {
+            let left = bst(l, i-1)
+            let right = bst(i+1,r)
+            for leftNode in left {
+                for rightNode in right {
+                    let root = TreeNode(i)
+                    root.left = leftNode
+                    root.right = rightNode
+                    res.append(root)
+                }
+            }
+        }
+        return res
+    }
+}
