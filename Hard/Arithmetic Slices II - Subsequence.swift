8f1474a3e768c2a04037df1e7a5d6cc393502f1f
zip520123 <zip520123@yahoo.com.tw>
2021-09-10 10:30:19 +0100

    Arithmetic Slices II - Subsequence

---
+++ Hard/Arithmetic Slices II - Subsequence.swift
@@ -0,0 +1,16 @@
+//Arithmetic Slices II - Subsequence
+//O(n^2), O(n^2)
+    func numberOfArithmeticSlices(_ nums: [Int]) -> Int {
+        var res = 0
+        var map = [[Int: Int]](repeating: [:], count: nums.count)
+        for i in 0..<nums.endIndex {
+            for j in 0..<i {
+                let diff = nums[j] - nums[i]
+                let c1 = map[i][diff, default:0]
+                let c2 = map[j][diff, default:0]
+                res += c2
+                map[i][diff,default:0] = c1+c2+1
+            }
+        }
+        return res
+    }
