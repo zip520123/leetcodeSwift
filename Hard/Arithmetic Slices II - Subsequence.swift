df7c578c48bb3cc8c318286c5578d6a85c43a123
zip520123 <zip520123@yahoo.com.tw>
2021-09-10 10:30:43 +0100

    Another solution

--- Hard/Arithmetic Slices II - Subsequence.swift
+++ Hard/Arithmetic Slices II - Subsequence.swift
@@ -14,3 +14,19 @@
         }
         return res
     }
+
+    //another solution
+    func numberOfArithmeticSlices(_ nums: [Int]) -> Int {
+        var res = 0
+        var map = [[Int:Int]](repeating: [:], count: nums.count)
+        for i in 0..<nums.endIndex {
+            for j in 0..<i {
+                let diff = nums[j] - nums[i]
+                let count = map[j][diff, default: 0]
+
+                res += count
+                map[i][diff, default: 0] += count + 1
+            }
+        }
+        return res
+    }
