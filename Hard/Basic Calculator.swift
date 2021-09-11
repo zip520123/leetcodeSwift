b0190084a665491cd8d9beb53c4d4b907257af40
zip520123 <zip520123@yahoo.com.tw>
2021-09-11 14:34:30 +0100

    Basic Calculator

---
+++ Hard/Basic Calculator.swift
@@ -0,0 +1,86 @@
+//Basic Calculator
+//O(n), O(n)
+    func calculate(_ s: String) -> Int {
+        var curr = 0, n = 0
+        var sign: Character = "+"
+        let sArr = Array(s)
+        var i = 0
+        var stack = [(curr:Int, sign:Character)]()
+        while i < sArr.endIndex {
+            let char = sArr[i]
+            switch char {
+                case "(":
+                    stack.append((curr,sign))
+                    curr = 0; n = 0; sign = "+"
+                case ")":
+                    let prev = stack.removeLast()
+                    if sign == "+" {
+                        curr += n
+                    } else {
+                        curr -= n
+                    }
+
+                    if prev.sign == "+" {
+                        curr = prev.curr + curr
+                    } else {
+                        curr = prev.curr - curr
+                    }
+                    n = 0; sign = "+"
+                case "+", "-":
+                    if sign == "+" {
+                        curr += n
+                    } else {
+                        curr -= n
+                    }
+                    n = 0; sign = char
+                
+                case "0"..."9":
+                    n = n*10 + Int(String(char))!
+                
+                default:
+                    break
+            }
+            i += 1
+        }
+        if sign == "+" {
+            curr += n
+        } else {
+            curr -= n
+        }
+        return curr 
+    }
+
+    //short version
+        func calculate(_ s: String) -> Int {
+        var curr = 0, n = 0
+        var sign: Character = "+"
+        let sArr = Array(s)
+        var i = 0
+        var stack = [(curr:Int, sign:Character)]()
+        while i < sArr.endIndex {
+            let char = sArr[i]
+            switch char {
+                case "(":
+                    stack.append((curr,sign))
+                    curr = 0; n = 0; sign = "+"
+                case ")":
+                    let prev = stack.removeLast()
+                    curr = sign == "+" ? curr + n : curr - n
+                    curr = prev.sign == "+" ? prev.curr + curr : prev.curr - curr
+                    
+                    n = 0; sign = "+"
+                case "+", "-":
+                    curr = sign == "+" ? curr + n : curr - n
+                    n = 0; sign = char
+                
+                case "0"..."9":
+                    n = n*10 + Int(String(char))!
+                
+                default:
+                    break
+            }
+            i += 1
+        }
+        curr = sign == "+" ? curr + n : curr - n
+        return curr 
+    }
