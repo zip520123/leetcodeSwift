83bbfb843f8e84a86c5ed7540d11b692885b5e1e
zip520123 <zip520123@yahoo.com.tw>
2021-10-07 19:01:01 +0100

    Word Search

--- Medium/Word Search.swift
+++ Medium/Word Search.swift
@@ -1,35 +1,32 @@
 /*Word Search*/
-func exist(_ board: [[Character]], _ word: String) -> Bool {
-    var wArr = Array(word)
-    let wcount = word.count
-    var board = board 
-    let row = board.count
-    let col = board[0].count
-    func dfs(_ index: Int,_ x: Int,_ y: Int) -> Bool {
-        if index == wcount {return true}
-        if x < 0 || x >= row || y < 0 || y >= col || board[x][y] == "*" || board[x][y] != wArr[index]{
+//O(board*word), O(board*word)
+    func exist(_ board: [[Character]], _ word: String) -> Bool {
+        let wArr = Array(word), rows = board.endIndex, cols = board[0].endIndex
+        var board = board
+        func dfs(_ x: Int, _ y: Int, _ index: Int) -> Bool {
+            if index == wArr.endIndex {return true}
+            guard x>=0, y>=0, x<rows, y<cols else {return false}
+            
+            if board[x][y] == wArr[index] {
+                let temp = board[x][y]
+                board[x][y] = "."
+                if dfs(x+1,y,index+1) == true {return true}
+                if dfs(x-1,y,index+1) == true {return true}
+                if dfs(x,y+1,index+1) == true {return true}
+                if dfs(x,y-1,index+1) == true {return true}
+                board[x][y] = temp
+             }
             return false
         }
-        let t = board[x][y]
-        board[x][y] = "*"
-        let r1 = dfs(index+1,x+1,y) || dfs(index+1,x-1,y) || dfs(index+1,x,y+1) || dfs(index+1,x,y-1)
-        board[x][y] = t
         
-        return r1
-    }
-    
-    for (i,row) in board.enumerated() {
-        for (j,char) in row.enumerated() {
-            if char == wArr[0] {
-                if dfs(0,i,j) == true {
-                    return true
-                }   
+        for i in 0..<rows {
+            for j in 0..<cols {
+                if dfs(i,j,0) == true {return true}
             }
         }
+        return false
     }
-    
-    return false
-}
+
 //Trie
 class Solution {
     class Trie {
