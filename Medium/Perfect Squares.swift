b6f6f62aba209bea106d0dd8aae4e37460dcb782
zip520123 <zip520123@yahoo.com.tw>
2021-10-14 21:16:43 +0100

    Perfect Squares

--- Medium/Perfect Squares.swift
+++ Medium/Perfect Squares.swift
@@ -1,9 +1,10 @@
 //Perfect Squares
+//O(n), O(n)
     func numSquares(_ n: Int) -> Int {
         var dp = [0]
         for i in 1...n {
             dp.append(i)
-            var j = 1
+            var j = 2
             while j*j <= i {
                 dp[i] = min(dp[i], dp[i-j*j] + 1)
                 j+=1
