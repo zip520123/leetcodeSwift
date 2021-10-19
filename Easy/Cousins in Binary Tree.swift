0bd31e863856dfec3de8a84642b04c5d2b3e589d
zip520123 <zip520123@yahoo.com.tw>
2021-10-19 01:10:04 +0100

    Cousins in Binary Tree

--- Easy/Cousins in Binary Tree.swift
+++ Easy/Cousins in Binary Tree.swift
@@ -1,18 +1,16 @@
 //Cousins in Binary Tree
 //O(n), O(n)
     func isCousins(_ root: TreeNode?, _ x: Int, _ y: Int) -> Bool {
-        var node1: TreeNode?, node2: TreeNode?
+        
         var d1: Int?, d2: Int?
         var parent1: TreeNode?, parent2: TreeNode?
         func dfs(_ node: TreeNode?, _ d: Int = 0) {
             guard let node = node else {return}
             if let leftNode = node.left {
                 if leftNode.val == x {
-                    node1 = leftNode
                     parent1 = node
                     d1 = d
                 } else if leftNode.val == y {
-                    node2 = leftNode
                     parent2 = node
                     d2 = d
                 }
@@ -20,11 +18,9 @@
             }
             if let rightNode = node.right {
                 if rightNode.val == y {
-                    node2 = rightNode
                     parent2 = node
                     d2 = d
                 } else if rightNode.val == x {
-                    node1 = rightNode
                     parent1 = node
                     d1 = d
                 }
@@ -35,10 +31,8 @@
         dfs(root)
         if let rootNode = root {
             if rootNode.val == x {
-                node1 = rootNode
                 d1 = -1
             } else if rootNode.val == y {
-                node2 = rootNode
                 d2 = -1
             }
         }
