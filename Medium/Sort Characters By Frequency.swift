72366faecb1f5f6f5567392b26ac995c3c66dfba
zip520123 <zip520123@yahoo.com.tw>
2021-10-22 09:04:59 +0100

    Sort Characters By Frequency

---
+++ Medium/Sort Characters By Frequency.swift
@@ -0,0 +1,16 @@
+//Sort Characters By Frequency
+//O(n), O(n)
+    func frequencySort(_ s: String) -> String {
+        var dict = [Character : Int]()
+        for char in s {
+            dict[char, default:0] += 1
+        }
+        var res = ""
+        let list = dict.sorted {$0.value > $1.value}
+        for item in list {
+            for n in 0..<item.value {
+                res.append(item.key)
+            }
+        }
+        return res
+    }
