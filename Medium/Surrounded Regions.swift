3c532c10660185b54c4b085c05011c7feec192c5
zip520123 <zip520123@yahoo.com.tw>
2021-11-01 17:56:13 +0000

    Surrounded Regions

--- Medium/Surrounded Regions.swift
+++ Medium/Surrounded Regions.swift
@@ -1,27 +1,30 @@
 /*Surrounded Regions*/
-//O(n)
+//O(n), O(n)
     func solve(_ board: inout [[Character]]) {
-        let row = board.endIndex, col = board[0].endIndex
+        let rows = board.endIndex, cols = board[0].endIndex
         func dfs(_ x:Int,_ y:Int) {
             guard x>=0, x<rows, y>=0, y<cols else {return}
-            board[x][y] = "*"
-            dfs(x+1,y)
-            dfs(x-1,y)
-            dfs(x,y+1)
-            dfs(x,y-1)
+            if board[x][y] == "O" {
+                board[x][y] = "*"
+                dfs(x+1,y)
+                dfs(x-1,y)
+                dfs(x,y+1)
+                dfs(x,y-1)    
+            }
+            
         }
         
-        for i in 0..<row {
+        for i in 0..<rows {
             dfs(i,0)
-            dfs(i,col-1)
+            dfs(i,cols-1)
         }
-        for i in 0..<col {
+        for i in 0..<cols {
             dfs(0,i)
-            dfs(row-1,i)
+            dfs(rows-1,i)
         }
         
-        for i in 0..<row {
-            for j in 0..<col {
+        for i in 0..<rows {
+            for j in 0..<cols {
                 if board[i][j] == "*" {
                     board[i][j] = "O"   
                 } else if board[i][j] == "O" {
