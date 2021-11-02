7b27ded1dfd60b5fcc4c9b37640779f56dcc0076
zip520123 <zip520123@yahoo.com.tw>
2021-11-02 18:26:13 +0000

    Unique Paths III

--- Hard/Unique Paths III.swift
+++ Hard/Unique Paths III.swift
@@ -1,4 +1,5 @@
 /*Unique Paths III*/
+//O(n^2), O(n^2)
     func uniquePathsIII(_ grid: [[Int]]) -> Int {
         var res = 0, empty = 1, sx = 0, sy = 0, ex = 0, ey = 0
         let row = grid.endIndex, col = grid[0].endIndex
@@ -31,5 +32,44 @@
         }
         
         dfs(sx,sy,empty)
+        return res
+    }
+
+    func uniquePathsIII(_ grid: [[Int]]) -> Int {
+        var sx = 0, sy = 0, empty = 0
+        let rows = grid.endIndex, cols = grid[0].endIndex
+        for i in 0..<rows {
+            for j in 0..<cols {
+                if grid[i][j] == 1 {
+                    sx = i; sy = j
+                }
+                if grid[i][j] == 0 {
+                    empty += 1
+                }
+            }
+        }
+        var res = 0, grid = grid
+        func dfs(_ x: Int, _ y: Int) {
+            guard x>=0, x<rows, y>=0, y<cols else {return}
+            if grid[x][y] == 2 {
+                if empty == 0 {
+                    res += 1
+                }
+            }
+            if grid[x][y] == 0 {
+                grid[x][y] = -1
+                empty -= 1
+                dfs(x+1,y)
+                dfs(x-1,y)
+                dfs(x,y+1)
+                dfs(x,y-1)
+                grid[x][y] = 0
+                empty += 1
+            }
+        }
+        dfs(sx+1,sy)
+        dfs(sx-1,sy)
+        dfs(sx,sy+1)
+        dfs(sx,sy-1)
         return res
     }
