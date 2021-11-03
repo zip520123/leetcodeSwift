85bf972f587e846f187c74a53592dde95830b561
zip520123 <zip520123@yahoo.com.tw>
2021-11-03 16:50:24 +0000

    Sum Root to Leaf Numbers

--- Medium/Sum Root to Leaf Numbers.swift
+++ Medium/Sum Root to Leaf Numbers.swift
@@ -2,16 +2,18 @@
 //O(n) O(h)
     func sumNumbers(_ root: TreeNode?) -> Int {
         var res = 0
-        func dfs(_ node: TreeNode?, _ path: String) {
+        
+        func dfs(_ node: TreeNode?, _ s: String) {
             guard let node = node else {return}
+            let n = s + String(node.val)
             if node.left == nil && node.right == nil {
-                let n = Int(path + "\(node.val)")!
-                res += n
-                return 
+                res += Int(n)!
             }
-            dfs(node.left, path + "\(node.val)")
-            dfs(node.right, path + "\(node.val)")
+            dfs(node.left, n)
+            dfs(node.right, n)
+            
         }
         dfs(root, "")
+        
         return res
     }
