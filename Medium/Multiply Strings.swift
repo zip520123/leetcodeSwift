c080faf0e4fbe779b4facb5ca42e3f6df15b3ecb
zip520123 <zip520123@yahoo.com.tw>
2021-11-07 12:00:54 +0000

    Multiply Strings

--- Medium/Multiply Strings.swift
+++ Medium/Multiply Strings.swift
@@ -24,3 +24,23 @@ func multiply(_ num1: String, _ num2: String) -> String {
     }
     return res == "" ? "0" : res
 }
+
+    func multiply(_ num1: String, _ num2: String) -> String {
+        let n1 = Array(num1), n2 = Array(num2)
+        var arr = [Int](repeating: 0, count: n1.endIndex + n2.endIndex)
+        for i in (0..<n1.endIndex).reversed() {
+            for j in (0..<n2.endIndex).reversed() {
+                let mul = Int(String(n1[i]))! * Int(String(n2[j]))!
+                let curr = i+j+1, ten = i+j
+                let sum = arr[curr] + mul
+                arr[curr] = sum % 10
+                arr[ten] += sum / 10
+            }
+        }
+        var res = ""
+        for n in arr {
+            if n == 0 && res == "" {continue}
+            res += String(n)
+        }
+        return res == "" ? "0" : res
+    }
