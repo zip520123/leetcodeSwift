378a416fd62a5185d9c959629407fbc9b9a63efe
zip520123 <zip520123@yahoo.com.tw>
2021-12-14 13:06:03 +0000

    Range Sum of BST

--- Easy/Range Sum of BST.swift
+++ Easy/Range Sum of BST.swift
@@ -30,3 +30,31 @@
         }
         return res
     }
+
+    func rangeSumBST(_ root: TreeNode?, _ low: Int, _ high: Int) -> Int {
+        guard let node = root else {return 0}
+        
+        let left = rangeSumBST(node.left, low, high)
+        let right = rangeSumBST(node.right, low, high)
+        let curr = node.val >= low && node.val <= high ? node.val : 0
+        return left + right + curr
+    }
+
+    func rangeSumBST(_ root: TreeNode?, _ low: Int, _ high: Int) -> Int {
+        guard let node = root else {return 0}
+        var queue = [node]
+        var res = 0
+        while !queue.isEmpty {
+            let node = queue.removeFirst()
+            if node.val >= low && node.val <= high {
+                res += node.val
+            }
+            if let left = node.left {
+                queue.append(left)
+            }
+            if let right = node.right {
+                queue.append(right)
+            }
+        }
+        return res
+    }
