9449b67761f836a583a3d8e424e2280cfc0eb904
zip520123 <zip520123@yahoo.com.tw>
2021-12-26 12:04:03 +0000

    K Closest Points to Origin

--- Medium/K Closest Points to Origin.swift
+++ Medium/K Closest Points to Origin.swift
@@ -35,3 +35,35 @@
         quickSelect(0, points.endIndex-1)
         return Array(points[0..<k])
     }
+
+//O(n^2), O(1)
+class Solution {
+    func kClosest(_ points: [[Int]], _ k: Int) -> [[Int]] {
+        var points = points
+        var l = 0, r = points.endIndex-1
+        while true {
+            
+            for i in l..<r {
+                if dist(points[i]) < dist(points[r]) {
+                    (points[l], points[i]) = (points[i], points[l])
+                    l += 1
+                }
+            }
+            (points[l], points[r]) = (points[r], points[l])
+            
+            if l+1 > k {
+                r = l-1
+                l = 0
+            } else if l+1 < k {
+                l+=1
+            } else {
+                break
+            }
+        }
+        return Array(points[0..<k])
+    }
+    
+    func dist(_ p1:[Int]) -> Int {
+        p1[0]*p1[0]+p1[1]*p1[1]
+    }
+}
