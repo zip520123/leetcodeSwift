cbc49b3ebd0f2ca733b1307bab7451de5dda8069
zip520123 <zip520123@yahoo.com.tw>
2021-12-26 12:11:03 +0000

    Basic Calculator II

--- Medium/Basic Calculator II.swift
+++ Medium/Basic Calculator II.swift
@@ -33,3 +33,34 @@
         
         return sum + lastVal
     }
+
+//O(n),O(1)
+    func calculate(_ s: String) -> Int {
+        var s = s + "++"
+        var curr = 0, last = 0, sum = 0, sign: Character = "+"
+        
+        for char in s {
+            if char == " " {continue}
+            if let n = Int(String(char)) {
+                curr = curr*10 + n
+            } else {
+                switch sign {
+                    case "+":
+                        sum += last
+                        last = curr
+                    case "-":
+                        sum += last
+                        last = -curr
+                    case "*":
+                        last *= curr
+                    case "/":
+                        last /= curr
+                    default:
+                        break
+                }
+                curr = 0
+                sign = char
+            }
+        }
+        return sum
+    }
