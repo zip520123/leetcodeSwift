1999ccabea5e41969cf3ced9421b6fb45e6fc87a
zip520123 <zip520123@yahoo.com.tw>
2021-12-28 11:48:06 +0000

    Middle of the Linked List

---
+++ Easy/Middle of the Linked List.swift
@@ -0,0 +1,18 @@
+//Middle of the Linked List
+//O(2n), O(1)
+    func middleNode(_ head: ListNode?) -> ListNode? {
+        var curr = head
+        var count = 0
+        while curr != nil {
+            count += 1
+            curr = curr!.next
+        }
+        count /= 2   
+        
+        
+        curr = head
+        for i in 0..<count {
+            curr = curr!.next
+        }
+        return curr
+    }
