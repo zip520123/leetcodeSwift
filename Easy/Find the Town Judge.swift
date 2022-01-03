48e63f4cb079ad36612fdf1ecfaab4d14beacfac
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-01-03 10:31:39 +0000

    Find the Town Judge

--- Easy/Find the Town Judge.swift
+++ Easy/Find the Town Judge.swift
@@ -1,14 +1,14 @@
 //Find the Town Judge
-//O(n), O(n)
+//O(n), O(n^2)
     func findJudge(_ n: Int, _ trust: [[Int]]) -> Int {
-        var dict = [Int: Set<Int>](), people = Set<Int>((1...n))
+        var dict = [Int: Set<Int>](), peopleDontTrust = Set<Int>((1...n))
         for t in trust {
             dict[t[1], default: []].insert(t[0])
-            people.remove(t[0])
+            peopleDontTrust.remove(t[0])
         }
-        for i in 1...n {
-            if dict[i, default:[]].count == n-1, people.contains(i) {
-                return i
+        for person in 1...n {
+            if dict[person, default:[]].count == n-1, peopleDontTrust.contains(person) {
+                return person
             }
         }
         return -1
