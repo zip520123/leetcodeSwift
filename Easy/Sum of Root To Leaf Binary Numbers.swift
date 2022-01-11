06c7be4663bee4334529a591e83e15bae5b0da74
zip520123 <zip520123@yahoo.com.tw>
2022-01-11 19:17:25 +0000

    Sum of Root To Leaf Binary Numbers

--- Easy/Sum of Root To Leaf Binary Numbers.swift
+++ Easy/Sum of Root To Leaf Binary Numbers.swift
@@ -14,3 +14,21 @@
         dfs(root,0)
         return sum
     }
+    
+//O(n), O(h)
+    func sumRootToLeaf(_ root: TreeNode?) -> Int {
+        var res = 0
+        
+        func dfs(_ path: [Int], _ node: TreeNode?) {
+            guard let node = node else {return}
+            if node.left == nil && node.right == nil {
+                let curr = Int((path + [node.val]).map { String($0) }.joined(), radix: 2) ?? 0
+                res += curr
+                return
+            }
+            dfs(path+[node.val], node.left)
+            dfs(path+[node.val], node.right)
+        }
+        dfs([], root)
+        return res
+    }
