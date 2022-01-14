1dd3d5c0d7a2c95e42ca9151d32ef9fa312dc1ca
zip520123 <zip520123@yahoo.com.tw>
2022-01-14 22:07:50 +0000

    atoi

--- Medium/String to Integer (atoi).swift
+++ Medium/String to Integer (atoi).swift
@@ -41,3 +41,82 @@ func myAtoi(_ s: String) -> Int {
     }
     return value
 }
+//O(n), O(1)
+    func myAtoi(_ s: String) -> Int {
+        var res = 0
+        var negative = false
+        var i = 0
+        let arr = Array(s)
+        while i<s.count, arr[i] == " " {
+            i += 1
+        }
+        if i<s.count, arr[i] == "+" || arr[i] == "-" {
+            if arr[i] == "-" {negative = true}
+            i += 1    
+        }
+        while i<s.count, let n = Int(String(arr[i])) {
+            res = res * 10 + n
+            if res >= Int(Int32.max) {
+                break
+            }
+            i += 1
+        }
+        res = negative ? -res : res
+        res = res > Int(Int32.max) ? Int(Int32.max) : res
+        res = res < Int(Int32.min) ? Int(Int32.min) : res
+        return res
+    }
+    
+//DFA
+//O(n), O(1)
+    enum Action {
+        case space, sign, num
+    }
+    func myAtoi(_ s: String) -> Int {
+        let dfa: [[Action:Int]] = [
+            [.space: 0, .sign:1, .num: 2],
+            [.num: 2],
+            [.num: 2]
+        ]
+        
+        var res = 0
+        var state = 0
+        var negative = false
+        loop: for char in s {
+            switch char {
+                case " ": //space
+                    if let next = dfa[state][.space] {
+                        state = next
+                    } else {
+                        break loop
+                    }
+                case "0"..."9": //num
+                    if let next = dfa[state][.num] {
+                        let n = Int(String(char))!
+                        res = res*10 + n
+                        state = next
+                        if res >= Int(Int32.max) {
+                            break loop
+                        }
+                    } else {
+                        break loop
+                    }
+                case "+", "-": //sign
+                    if let next = dfa[state][.sign] {
+                        if char == "-" {
+                            negative = true
+                        }
+                        state = next
+                    } else {
+                        break loop
+                    }
+                default:
+                    break loop
+            }
+        }
+        res = negative ? -res : res
+        res = res > Int(Int32.max) ? Int(Int32.max) : res
+        res = res < Int(Int32.min) ? Int(Int32.min) : res
+        return res
+    }
+
