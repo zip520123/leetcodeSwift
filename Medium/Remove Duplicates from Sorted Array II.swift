05708e4a524fcccec86bcab929a760f37bd6d755
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-02-06 15:49:04 +0000

    Remove Duplicates from Sorted Array II

--- Medium/Remove Duplicates from Sorted Array II.swift
+++ Medium/Remove Duplicates from Sorted Array II.swift
@@ -1,6 +1,18 @@
 //Remove Duplicates from Sorted Array II
 //O(n), O(1)
     func removeDuplicates(_ nums: inout [Int]) -> Int {
+        var i = 0
+        for n in nums {
+            if i < 2 || n > nums[i-2] {
+                nums[i] = n
+                i += 1
+            }
+        }
+        return i
+    }
+
+//O(n), O(1)
+    func removeDuplicates(_ nums: inout [Int]) -> Int {
         var l = 0, r = 0, count = 0
         while r<nums.endIndex {
             if nums[l] == nums[r] {
