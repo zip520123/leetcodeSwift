bb968dbb4e86d7dd9f76d372f5381f2e2d489f2d
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-02-07 12:14:37 +0000

    Find the Difference

---
+++ Easy/Find the Difference.swift
@@ -0,0 +1,18 @@
+//Find the Difference
+//O(s+t), O(s+t)
+    func findTheDifference(_ s: String, _ t: String) -> Character {
+        var sdict = [Character: Int]()
+        for char in s {
+            sdict[char, default:0] += 1
+        }
+        var tdict = [Character: Int]()
+        for char in t {
+            tdict[char, default:0] += 1
+        }
+        for (char, val) in tdict {
+            if sdict[char, default: 0] != val {
+                return char
+            }
+        }
+        return Character("")
+    }
