7b6307f138e9c3cbe5290c4d26a5f77a5ef58f82
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-02-11 13:45:58 +0000

    Permutation in String

--- Medium/Permutation in String.swift
+++ Medium/Permutation in String.swift
@@ -2,30 +2,25 @@
 //O(s1+s2) O(s1)
     func checkInclusion(_ s1: String, _ s2: String) -> Bool {
         var dict = [Character: Int]()
-        var count = s1.count
         for char in s1 {
             dict[char, default:0] += 1
         }
-        let sArr = Array(s2)
+        var count = s1.count
         var l = 0, r = 0
+        let sArr = Array(s2)
         while r < sArr.endIndex {
-            let char = sArr[r]
-            dict[char, default:0] -= 1
-            if dict[char,default:0] >= 0 {
-                count -= 1
-                if count == 0 {
-                    return true
-                }
+            dict[sArr[r], default: 0] -= 1
+            if dict[sArr[r]]! >= 0 {
+                count -= 1  
             }
-            while dict[char,default:0] < 0 {
-                let leftChar = sArr[l]
-                dict[leftChar]! += 1
-                if dict[leftChar]! > 0 {
-                    count += 1
-                }
+            while dict[sArr[r]]! < 0 {
+                dict[sArr[l]]! += 1
+                if dict[sArr[l]]! > 0 {count += 1}
                 l += 1
             }
+            if count == 0 {return true}
             r += 1
         }
         return false
+        
     }
