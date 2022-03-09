a561c212597753509e6a47703ec6e9fc7bd5e2d3
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-03-09 18:33:33 +0000

    Remove Duplicates from Sorted List II

--- Easy/Remove Duplicates from Sorted List II.swift
+++ Easy/Remove Duplicates from Sorted List II.swift
@@ -1,4 +1,5 @@
 //Remove Duplicates from Sorted List II
+//O(n), O(1)
     func deleteDuplicates(_ head: ListNode?) -> ListNode? {
         
         var myHead = ListNode(-1000)
@@ -19,4 +20,28 @@
             
         }
         return myHead.next
+    }
+
+//O(n), O(n)
+    func deleteDuplicates(_ head: ListNode?) -> ListNode? {
+        var dict = [Int: Int]()
+        var curr = head
+        while curr != nil {
+            dict[curr!.val, default: 0] += 1
+            curr = curr!.next
+        }
+        let res = ListNode()
+        var node = res
+        curr = head
+        while curr != nil {
+            let key = curr!.val
+            let val = dict[key]!
+            if val == 1 {
+                let next = ListNode(key)
+                node.next = next
+                node = node.next!
+            }
+            curr = curr!.next
+        }
+        return res.next
     }
