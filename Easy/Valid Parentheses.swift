9f9d50c064904b41b42f483df8ecc666822040ec
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-03-13 10:43:59 +0000

    Valid Parentheses

--- Easy/Valid Parentheses.swift
+++ Easy/Valid Parentheses.swift
@@ -1,4 +1,5 @@
 /*Valid Parentheses*/
+//O(n), O(n)
 func isValid(_ s: String) -> Bool {
     var arr = [Character]()
     
@@ -29,3 +30,28 @@ func isValid(_ s: String) -> Bool {
     
     return arr.isEmpty
 }
+
+    func isValid(_ s: String) -> Bool {
+        var stack = [Character]()
+        for char in s {
+            switch char {
+                case "(", "[", "{":
+                    stack.append(char)
+                case ")":
+                    if stack.last == nil {return false}
+                    let last = stack.removeLast()
+                    if last != "(" {return false}
+                case "]":
+                    if stack.last == nil {return false}
+                    let last = stack.removeLast()
+                    if last != "[" {return false}
+                case "}":
+                    if stack.last == nil {return false}
+                    let last = stack.removeLast()
+                    if last != "{" {return false}
+                default:
+                    break
+            }
+        }
+        return stack.isEmpty
+    }
