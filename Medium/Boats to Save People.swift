1b7a645f031dcdb068cd58d18042a08c9308a396
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-03-24 11:17:24 +0000

    Boats to Save People

---
+++ Medium/Boats to Save People.swift
@@ -0,0 +1,15 @@
+//Boats to Save People
+//O(n), O(1)
+    func numRescueBoats(_ people: [Int], _ limit: Int) -> Int {
+        let arr = people.sorted()
+        var i = 0, j = arr.endIndex-1
+        var res = 0
+        while i<=j {
+            res += 1
+            if arr[i] + arr[j] <= limit {
+                i += 1
+            }    
+            j -= 1
+        }
+        return res
+    }
