1a0f90eb04d1f8e6e5c20eeec0e1eff8b078f97d
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-04-13 10:20:06 +0100

    Spiral Matrix II

--- Medium/Spiral Matrix II.swift
+++ Medium/Spiral Matrix II.swift
@@ -41,3 +41,46 @@
         }
         return res
     }
+
+//O(n), O(1)
+    func generateMatrix(_ n: Int) -> [[Int]] {
+        var res = (0..<n).map {_ in (0..<n).map {_ in 0 }}
+        var left = 0, top = 0, right = n-1, bottom = n-1
+        var d = Direction.right
+        var i = 1
+        while i <= n*n {
+            switch d {
+                case .right:
+                    for col in left...right {
+                        res[top][col] = i    
+                        i += 1
+                    }
+                    d = .bottom
+                    top += 1
+
+                case .bottom:
+                    for row in top...bottom {
+                        res[row][right] = i
+                        i += 1
+                    }
+                    right-=1
+                    d = .left
+                    
+                case .left:
+                    for col in (left...right).reversed() {
+                        res[bottom][col] = i
+                        i += 1
+                    }
+                    bottom -= 1
+                    d = .top
+                case .top:
+                    for row in (top...bottom).reversed() {
+                        res[row][left] = i
+                        i += 1
+                    }
+                    left += 1
+                    d = .right
+            }
+        }
+        return res
+    }
