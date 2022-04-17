292f5aa9b0efa61c0033ffdea57016828df2c764
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-04-17 09:32:41 +0100

    Convert BST to Greater Tree

--- Medium/Convert BST to Greater Tree.swift
+++ Medium/Convert BST to Greater Tree.swift
@@ -12,3 +12,20 @@
         dfs(root)
         return root
     }
+
+    func convertBST(_ root: TreeNode?) -> TreeNode? {
+        var node = root
+        var queue = [TreeNode]()
+        var curr = 0
+        while node != nil || !queue.isEmpty { 
+            while node != nil {
+                queue.append(node!)
+                node = node!.right
+            }
+            node = queue.removeLast()
+            node!.val += curr
+            curr = node!.val
+            node = node!.left
+        }
+        return root
+    }
