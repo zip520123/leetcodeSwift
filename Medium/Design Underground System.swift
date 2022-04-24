02189bc44991a08ed079353eb907198e8b56a020
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-04-24 09:32:09 +0100

    Design Underground System

---
+++ Medium/Design Underground System.swift
@@ -0,0 +1,27 @@
+//Design Underground System
+
+class UndergroundSystem {
+    typealias ID = Int
+    typealias Time = Int
+    typealias Station = String
+    typealias StartNameToEndNameStation = String
+    
+    var checkIn = [ID: (Time, Station)]()
+    var average = [StartNameToEndNameStation: [Time]]() //O(startStation*endStation)
+    
+    func checkIn(_ id: Int, _ stationName: String, _ t: Int) { //O(1)
+        checkIn[id] = (t, stationName)
+    }
+    
+    func checkOut(_ id: Int, _ stationName: String, _ t: Int) { //O(1)
+        let (checkInTime, checkInName) = checkIn[id]!
+        average["\(checkInName)-\(stationName)", default: []].append(t-checkInTime)
+    }
+    
+    func getAverageTime(_ startStation: String, _ endStation: String) -> Double {//O(startStation*endStation) * arr.len
+        let arr = average["\(startStation)-\(endStation)", default: []]
+        let sum = arr.reduce(0, +)
+        let res = Double(sum) / Double(arr.count)
+        return res
+    }
+}
