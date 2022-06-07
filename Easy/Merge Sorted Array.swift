b37be96927cd80f2c21819ec8d349f3ee579d996
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-06-07 09:57:18 +0100

    Merge Sorted Array

--- Easy/Merge Sorted Array.swift
+++ Easy/Merge Sorted Array.swift
@@ -6,3 +6,35 @@
          }
         nums1 = nums1.sorted()
     }
+
+    func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        if n == 0 { return }
+        if m == 0 {
+            nums1 = nums2
+            return}
+        nums1 = Array(nums1[0..<m])
+        
+        func mergeSort(_ arr1: [Int], _ arr2: [Int]) -> [Int] {
+            var l = 0, r = 0
+            var res = [Int]()
+            while l < arr1.endIndex && r < arr2.endIndex {
+                if arr1[l] < arr2[r] {
+                    res.append(arr1[l])
+                    l+=1
+                } else {
+                    res.append(arr2[r])
+                    r+=1
+                }
+            }
+            while l < arr1.endIndex {
+                res.append(arr1[l])
+                l+=1
+            }
+            while r < arr2.endIndex {
+                res.append(arr2[r])
+                r += 1
+            }
+            return res
+        }
+        nums1 = mergeSort(nums1,nums2)
+    }
