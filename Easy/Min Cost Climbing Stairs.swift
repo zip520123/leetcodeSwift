6f584a23b43aba3fa952b1da2dbff2de67288c0b
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-07-10 10:00:14 +0100

    Min Cost Climbing Stairs

--- Easy/Min Cost Climbing Stairs.swift
+++ Easy/Min Cost Climbing Stairs.swift
@@ -17,3 +17,30 @@ func minCostClimbingStairs(_ cost: [Int]) -> Int {
     }
     return dp.last!
 }
+
+//O(n), O(n)
+func minCostClimbingStairs(_ cost: [Int]) -> Int {
+    var arr = cost
+    for i in 2..<arr.endIndex {
+        arr[i] += min(arr[i-1],arr[i-2])
+    }
+    return min(arr[arr.endIndex-1], arr[arr.endIndex-2])
+}
+
+//O(n), O(n) Jump Game VI, queue
+    func minCostClimbingStairs(_ cost: [Int]) -> Int {
+        var dp = [0] + cost + [0]
+        var queue = [0]
+        for i in 1..<dp.endIndex {
+            let curr = dp[queue.first!] + dp[i]
+            dp[i] = curr
+            while !queue.isEmpty && dp[queue.last!] > curr {
+                queue.removeLast()
+            }
+            queue.append(i)
+            if queue.first! == i-2 {
+                queue.removeFirst()
+            }
+        }
+        return dp.last!
+    }
