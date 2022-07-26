67c00559626fcd8141661cbc274341ff77b5886b
zip520123 <zip520123@yahoo.com.tw>
2022-07-26 10:26:57 +0100

    Lowest Common Ancestor of a Binary Tree

--- Easy/Lowest Common Ancestor of a Binary Tree.swift
+++ Easy/Lowest Common Ancestor of a Binary Tree.swift
@@ -9,3 +9,15 @@
         if left == nil {return right}
         return left
     }
+
+//O(n), O(h)
+    func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
+        guard let node = root else {return nil}
+        if node === p || node === q {return node}
+        let left = lowestCommonAncestor(node.left, p,q)
+        let right = lowestCommonAncestor(node.right,p,q)
+        if left != nil && right != nil {return node}
+        if left != nil {return left}
+        if right != nil {return right}
+        return nil
+    }
