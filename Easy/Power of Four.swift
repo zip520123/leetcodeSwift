afdec1dbc299db0d104998697b62c7705d67a9dd
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-08-22 09:26:39 +0100

    Power of Four

---
+++ Easy/Power of Four.swift
@@ -0,0 +1,21 @@
+//Power of Four
+//O(log n), O(1)
+    func isPowerOfFour(_ n: Int) -> Bool {
+        if n == 0 {return false}
+        var i = 0, curr = 1
+        while true {
+            let curr = Int(pow(Double(4), Double(i))) 
+            if n == curr {return true} 
+            else if n < curr {
+                return false
+            }
+            i += 1
+        }
+        return false
+    }
+
+//O(1), O(1)
+    func isPowerOfFour(_ n: Int) -> Bool {
+        let mask = Int("1010101010101010101010101010101", radix: 2)!
+        return n > 0 && n & (n-1) == 0 && (mask & n) == n
+    }
