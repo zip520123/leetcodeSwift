e01244a1e0fd870a175d5a401ab58f02a5ab8a3b
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-09-06 10:33:19 +0100

    Binary Tree Pruning

--- Medium/Binary Tree Pruning.swift
+++ Medium/Binary Tree Pruning.swift
@@ -10,3 +10,16 @@
         if left == nil && right == nil && node.val == 0 {return nil}
         return newNode
     }
+
+//O(n), O(h)
+    func pruneTree(_ root: TreeNode?) -> TreeNode? {
+        guard let node = root else {return nil}
+        let left = pruneTree(node.left)
+        let right = pruneTree(node.right)
+        if node.val == 0 && left == nil && right == nil {
+            return nil
+        }
+        node.left = left
+        node.right = right
+        return node
+    }
