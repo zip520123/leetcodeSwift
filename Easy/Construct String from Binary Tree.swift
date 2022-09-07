fc90e8a4fd5a2dfb8899c837c9b76e2661a52791
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-09-07 09:18:30 +0100

    Construct String from Binary Tree

--- Easy/Construct String from Binary Tree.swift
+++ Easy/Construct String from Binary Tree.swift
@@ -18,6 +18,19 @@
 //O(n), O(h)
     func tree2str(_ root: TreeNode?) -> String {
         guard let node = root else {return ""}
+        var res = String(node.val)
+        if node.left != nil || node.right != nil {
+            res += "(" + tree2str(node.left) + ")"
+            if node.right != nil {
+                res += "(" + tree2str(node.right) + ")"
+            }
+        }
+        return res
+    }
+
+//O(n), O(h)
+    func tree2str(_ root: TreeNode?) -> String {
+        guard let node = root else {return ""}
         let left = tree2str(node.left)
         let right = tree2str(node.right)
         var res = String(node.val)
