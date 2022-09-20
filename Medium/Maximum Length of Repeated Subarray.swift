89cb918e6db8d53a64161631f28857394603b1e4
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-09-20 10:08:40 +0100

    Maximum Length of Repeated Subarray

--- Medium/Maximum Length of Repeated Subarray.swift
+++ Medium/Maximum Length of Repeated Subarray.swift
@@ -15,6 +15,23 @@ class Solution {
         return res
     }
 }
+
+//O(n1*n2), O(n1*n2)
+    func findLength(_ nums1: [Int], _ nums2: [Int]) -> Int {
+        var dp = [[Int]](repeating: [Int](repeating: 0, count: nums1.endIndex+1), count: nums2.endIndex+1)
+        var res = 0
+        for row in 1...nums2.endIndex {
+            for col in 1...nums1.endIndex {
+                if nums1[col-1] == nums2[row-1] {
+                    dp[row][col] = dp[row-1][col-1] + 1
+                    res = max(res, dp[row][col])
+                } else {
+                    dp[row][col] = 0
+                }
+            }
+        }
+        return res
+    }
 /*
 [1,2,3,2,1,3,2,1,4] n1
 [3,2,1,4,7] n2
