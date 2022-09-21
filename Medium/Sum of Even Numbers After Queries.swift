c3859c49dc7aee886defb11633b7fe2fd77d9e02
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-09-21 10:04:53 +0100

    Sum of Even Numbers After Queries

--- Medium/Sum of Even Numbers After Queries.swift
+++ Medium/Sum of Even Numbers After Queries.swift
@@ -23,3 +23,22 @@
         }
         return res
     }
+
+//O(n), O(n)
+    func sumEvenAfterQueries(_ nums: [Int], _ queries: [[Int]]) -> [Int] {
+        var nums = nums
+        var sum = nums.reduce(0, {$0 + ($1%2 == 0 ? $1 : 0) })
+        var res = [Int]()
+        for q in queries {
+            var i = q[1], val = q[0]
+            if nums[i] % 2 == 0 {
+                sum -= nums[i]
+            }
+            nums[i] += val
+            if nums[i] % 2 == 0 {
+                sum += nums[i]
+            }
+            res.append(sum)
+        }
+        return res
+    }
