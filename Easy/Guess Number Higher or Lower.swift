dcc3926ac1e1ee5c3db94cd3b6df5a8f16d75d2d
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-11-17 16:12:27 +0800

    Guess Number Higher or Lower

--- Easy/Guess Number Higher or Lower.swift
+++ Easy/Guess Number Higher or Lower.swift
@@ -1,4 +1,5 @@
 /*Guess Number Higher or Lower*/
+//O(log n), O(1)
 func guessNumber(_ n: Int) -> Int {
     
     var l = 1, r = n
@@ -8,6 +9,22 @@ func guessNumber(_ n: Int) -> Int {
             l = mid + 1
         } else {
             r = mid
+        }
+    }
+    return l
+}
+
+func guessNumber(_ n: Int) -> Int {
+    var l = 1, r = n
+    while l<r {
+        let mid = l+(r-l)>>1
+        let curr = guess(mid)
+        if curr == 1 {
+            l = mid+1
+        } else if curr == -1 {
+            r = mid-1
+        } else {
+            return mid
         }
     }
     return l
