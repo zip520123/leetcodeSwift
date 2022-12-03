d0d28c16820c5065fd2f5aedea18d547738aa2ae
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-12-03 10:46:47 +0900

    Sort Characters By Frequency

--- Medium/Sort Characters By Frequency.swift
+++ Medium/Sort Characters By Frequency.swift
@@ -14,3 +14,25 @@
         }
         return res
     }
+
+//O(n log n), O(n)
+    func frequencySort(_ s: String) -> String {
+        var dict = [Character: Int]()
+        for char in s {
+            dict[char, default: 0] += 1
+        }
+        let arr = Array(dict).sorted(by: { 
+            if $0.value == $1.value {
+                return $0.key < $1.key
+            }
+            return $0.value > $1.value 
+        })
+        var res = ""
+        for (key, value) in arr {
+            for _ in 0..<value {
+                res += String(key)
+            }
+        }
+
+        return res
+    }
