5af6a9cf9de089fda0283047a0a2f9d53847214a
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-12-10 19:28:27 +0900

    Maximum Product of Splitted Binary Tree

--- Medium/Maximum Product of Splitted Binary Tree.swift
+++ Medium/Maximum Product of Splitted Binary Tree.swift
@@ -21,3 +21,20 @@ class Solution {
         return countAll(node.left) + node.val + countAll(node.right)
     }
 }
+
+//O(n), O(h)
+    func maxProduct(_ root: TreeNode?) -> Int {
+        var total = 0, res = 0
+        
+        func dfs(_ node: TreeNode?) -> Int {
+            guard let node = node else {return 0}
+
+            let left = dfs(node.left)
+            let right = dfs(node.right)
+            res = max(res, left * (total - left), right * (total - right))
+            return left + right + node.val
+        }
+        total = dfs(root)
+        dfs(root)
+        return res % Int(1E9+7)
+    }
