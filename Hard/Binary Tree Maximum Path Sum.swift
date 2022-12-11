efbfa09929641e43e0a6a4639897832fc2831391
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-12-11 14:57:31 +0900

    Binary Tree Maximum Path Sum

--- Hard/Binary Tree Maximum Path Sum.swift
+++ Hard/Binary Tree Maximum Path Sum.swift
@@ -11,3 +11,17 @@ func maxPathSum(_ root: TreeNode?) -> Int {
     dfs(root)
     return res
 }
+
+//O(n), O(h)
+    func maxPathSum(_ root: TreeNode?) -> Int {
+        var res = Int.min
+        func dfs(_ node: TreeNode?) -> Int {
+            guard let node = node else {return 0}
+            let left = dfs(node.left)
+            let right = dfs(node.right)
+            res = max(res, node.val + left + right, node.val, node.val+left, node.val+right)
+            return max(node.val, node.val+left, node.val+right)
+        }    
+        dfs(root)
+        return res
+    }
