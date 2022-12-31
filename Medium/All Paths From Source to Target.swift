d027f98d9135380a3b5acf4a81391c1813d75105
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-12-31 10:50:41 +0900

    All Paths From Source to Target

--- Medium/All Paths From Source to Target.swift
+++ Medium/All Paths From Source to Target.swift
@@ -18,3 +18,26 @@
         }
         return res
     }
+
+//DFS O(n), O(n)
+    func allPathsSourceTarget(_ graph: [[Int]]) -> [[Int]] {
+        var res = [[Int]]()
+        var dict = [Int: [Int]]()
+        for (i, edge) in graph.enumerated() {
+            dict[i] = edge
+        }
+        
+        let target = graph.endIndex-1
+        
+        func dfs(_ curr: [Int], _ node: Int) {
+            if node == target {
+                res.append(curr)
+                return
+            }    
+            for next in dict[node, default:[]] {
+                dfs(curr+[next], next)
+            }
+        }
+        dfs([0],0)
+        return res
+    }
