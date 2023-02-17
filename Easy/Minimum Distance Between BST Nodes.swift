72ea1874e37e0c45ff3885f53bde36812116e94b
Hsiang-Lin <zip520123@yahoo.com.tw>
2023-02-17 21:06:55 +0900

    Minimum Distance Between BST Nodes

--- Easy/Minimum Distance Between BST Nodes.swift
+++ Easy/Minimum Distance Between BST Nodes.swift
@@ -15,3 +15,20 @@
         }
         return res
     }
+
+//O(n), O(n)
+    func minDiffInBST(_ root: TreeNode?) -> Int {
+        var prev: TreeNode?
+        var res = Int.max
+        func dfs(_ node: TreeNode?) {
+            guard let node = node else {return}
+            dfs(node.left)
+            if let prevNode = prev {
+                res = min(res, abs(node.val - prevNode.val))
+            }
+            prev = node
+            dfs(node.right)
+        }
+        dfs(root)
+        return res
+    }
