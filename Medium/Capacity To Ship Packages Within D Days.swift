dd07d2dc0da45e3898c3b9c85d00b6a334a834ee
Hsiang-Lin <zip520123@yahoo.com.tw>
2023-02-22 18:34:48 +0900

    Capacity To Ship Packages Within D Days

---
+++ Medium/Capacity To Ship Packages Within D Days.swift
@@ -0,0 +1,34 @@
+//Capacity To Ship Packages Within D Days
+//O(n log n), O(1)
+class Solution {
+    func shipWithinDays(_ weights: [Int], _ days: Int) -> Int {
+        var total = 0, maxLoad = 0
+        for w in weights {
+            maxLoad = max(maxLoad, w)
+            total += w
+        }
+
+        func feaible(_ c: Int) -> Bool {
+            var daysNeeded = 1, curr = 0
+            for w in weights {
+                curr += w
+                if curr > c {
+                    daysNeeded += 1
+                    curr = w
+                }
+            }
+            return daysNeeded <= days
+        }
+
+        var l = maxLoad, r = total
+        while l<r {
+            let mid = l+(r-l)>>1
+            if feaible(mid) {
+                r = mid
+            } else {
+                l = mid+1
+            }
+        }
+        return l
+    }
+}
