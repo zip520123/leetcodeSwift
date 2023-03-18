491c7bdfabd5df069d8927a08f3345e89c9f0ac0
Hsiang-Lin <zip520123@yahoo.com.tw>
2023-03-18 17:44:21 +0000

    Design Browser History

--- Medium/Design Browser History.swift
+++ Medium/Design Browser History.swift
@@ -34,3 +34,33 @@ class BrowserHistory {
         return curr
     }
 }
+
+//
+class BrowserHistory {
+    var arr = [String]() //O(n)
+    var curr = 0, last = 0
+    init(_ homepage: String) {
+        arr.append(homepage)
+    }
+    
+    func visit(_ url: String) { //O(1)
+        curr += 1
+        
+        if curr >= arr.endIndex {
+            arr.append(url)
+        } else {
+            arr[curr] = url
+        }
+        last = curr
+    }
+    
+    func back(_ steps: Int) -> String { //O(1)
+        curr = max(0, curr-steps)
+        return arr[curr]
+    }
+    
+    func forward(_ steps: Int) -> String { //O(1)
+        curr = min(last, curr+steps)
+        return arr[curr]
+    }
+}
