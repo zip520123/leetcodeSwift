83300e3d45f0c3aacd4e1cd853b52755bebf556a
Hsiang-Lin <zip520123@yahoo.com.tw>
2023-04-13 16:03:44 +0100

    Validate Stack Sequences

--- Medium/Validate Stack Sequences.swift
+++ Medium/Validate Stack Sequences.swift
@@ -1,15 +1,38 @@
 //Validate Stack Sequences
 //O(pushed+popped), O(pushed)
     func validateStackSequences(_ pushed: [Int], _ popped: [Int]) -> Bool {
-        var i = 0, j = 0
-        var stack = [Int]()
-        while i < pushed.endIndex {
+        var stack = [Int](), j = 0
+        for i in 0..<pushed.endIndex {
             stack.append(pushed[i])
-            i += 1
-            while j < popped.endIndex && !stack.isEmpty && stack.last! == popped[j] {
+            while !stack.isEmpty && j<popped.endIndex && popped[j] == stack.last! {
                 stack.removeLast()
                 j += 1
             }
+        }
+        return stack.isEmpty
+    }
+
+//O(pushed+popped), O(pushed)
+    func validateStackSequences(_ pushed: [Int], _ popped: [Int]) -> Bool {
+        var stack = [Int]()
+        var i = 0, j = 0, n = pushed.endIndex
+        while i < n, j < n {
+            if stack.isEmpty {
+                stack.append(pushed[i])
+                i += 1
+            } else {
+                if stack.last! == popped[j] {
+                    stack.removeLast()
+                    j += 1
+                } else {
+                    stack.append(pushed[i])
+                    i += 1
+                }
+            }
+        }
+        while !stack.isEmpty, j < n, stack.last! == popped[j] {
+            stack.removeLast()
+            j += 1
         }
         return stack.isEmpty
     }
