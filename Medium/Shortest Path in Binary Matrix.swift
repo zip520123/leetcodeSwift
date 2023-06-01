76bb5eb827860b25012d73a8c721780cd335cc0b
Hsiang-Lin <zip520123@yahoo.com.tw>
2023-06-01 12:29:58 +0100

    Shortest Path in Binary Matrix

--- Medium/Shortest Path in Binary Matrix.swift
+++ Medium/Shortest Path in Binary Matrix.swift
@@ -28,3 +28,36 @@
         }
         return -1
     }
+
+//O(n), O(n)
+    func shortestPathBinaryMatrix(_ grid: [[Int]]) -> Int {
+        let rows = grid.endIndex, cols = grid[0].endIndex
+        if grid[0][0] == 1 || grid[rows-1][cols-1] == 1 {return -1}
+        var seen = Set<[Int]>()
+        var queue = [(0,0)]
+        var step = 0
+        while !queue.isEmpty {
+            let temp = queue
+            queue.removeAll()
+            step += 1
+            for (x,y) in temp {
+                if x == rows-1, y == cols-1 {return step}
+
+                for (dx, dy) in [(1,1), (1,-1),(-1,1), (-1,-1), (0,1),(1,0), (-1,0),(0,-1)] {
+                    let nextX = x+dx, nextY = y+dy
+                    if seen.contains([nextX,nextY]) {continue}
+                    
+                    if nextX < rows, 
+                       nextY < cols,
+                       nextX >= 0,
+                       nextY >= 0,
+                       grid[nextX][nextY] == 0 {
+                        queue.append((nextX, nextY))
+                    }
+                    seen.insert([nextX,nextY])
+                }
+            }
+            
+        }
+        return -1
+    }
