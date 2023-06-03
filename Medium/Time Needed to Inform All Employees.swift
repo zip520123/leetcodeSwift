66a4887ad05bd496e174de525b1282f9244d0ca6
Hsiang-Lin <zip520123@yahoo.com.tw>
2023-06-03 13:33:01 +0100

    Time Needed to Inform All Employees

--- Medium/Time Needed to Inform All Employees.swift
+++ Medium/Time Needed to Inform All Employees.swift
@@ -1,5 +1,5 @@
 // Time Needed to Inform All Employees
-//O(n), O(n)
+//DFS O(n), O(n)
     func numOfMinutes(_ n: Int, _ headID: Int, _ manager: [Int], _ informTime: [Int]) -> Int {
         var dict = [Int: [Int]]()
         for (i,m) in manager.enumerated() {
@@ -15,4 +15,23 @@
             return subTime + t
         }
         return dfs(headID)
+    }
+
+//BFS O(n), O(n)
+    func numOfMinutes(_ n: Int, _ headID: Int, _ manager: [Int], _ informTime: [Int]) -> Int {
+        var dict = [Int: [Int]]()
+        for (i,m) in manager.enumerated() {
+            dict[m, default:[]].append(i)
+        }
+        
+        var queue: [(Int, Int)] = [(headID, 0)]
+        var res = 0
+        while !queue.isEmpty {
+            let (node, t) = queue.removeFirst()
+            res = max(res, t)
+            for subNode in dict[node, default: []] {
+                queue.append((subNode, t+informTime[node]))
+            }
+        }
+        return res
     }
