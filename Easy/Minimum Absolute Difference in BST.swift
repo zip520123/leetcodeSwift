58e9f601e60b189bd02b7d42a601778fba6dd43c
Hsiang-Lin <zip520123@yahoo.com.tw>
2023-06-14 13:56:23 +0100

    Minimum Absolute Difference in BST

--- Easy/Minimum Absolute Difference in BST.swift
+++ Easy/Minimum Absolute Difference in BST.swift
@@ -14,3 +14,25 @@
         dfs(root)
         return res
     }
+
+//O(n),O(n)
+    func getMinimumDifference(_ root: TreeNode?) -> Int {
+        var node = root
+        var stack = [TreeNode]()
+        var prev: TreeNode?
+        var res = Int.max
+        while node != nil || !stack.isEmpty {
+
+            while node != nil {
+                stack.append(node!)
+                node = node!.left
+            }
+            node = stack.removeLast()
+            if let prev = prev {
+                res = min(res, abs(prev.val-node!.val))
+            }
+            prev = node
+            node = node!.right
+        }
+        return res
+    }
