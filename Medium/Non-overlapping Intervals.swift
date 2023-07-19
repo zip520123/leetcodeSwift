7982ab7d568b472eb3c7581292904bc8d7ed1767
Hsiang-Lin <zip520123@yahoo.com.tw>
2023-07-19 11:31:27 +0100

    Non-overlapping Intervals

--- Medium/Non-overlapping Intervals.swift
+++ Medium/Non-overlapping Intervals.swift
@@ -90,3 +90,26 @@ the longer interval will always overlap with more or the same number of future i
         }
         return res
     }
+
+//O(n), O(1)
+    func eraseOverlapIntervals(_ intervals: [[Int]]) -> Int {
+        let arr = intervals.sorted { a,b in 
+            if a[0] == b[0] {
+                return a[1] < b[1]
+            }
+            return a[0] < b[0]
+        }
+        var l = 0, r = 1, res = 0
+        while r < arr.endIndex {
+            if arr[l][1] <= arr[r][0] {
+                l = r
+            } else {
+                res += 1
+                if arr[l][1] > arr[r][1] {
+                    l = r
+                }
+            }
+            r += 1
+        }
+        return res
+    }
