478db1f3f29c8f596b681ef1ee21b9a7c8e89e5c
Hsiang-Lin <zip520123@yahoo.com.tw>
2023-08-04 11:00:01 +0100

    Word Break

--- Medium/Word Break.swift
+++ Medium/Word Break.swift
@@ -23,3 +23,49 @@ func wordBreak(_ s: String, _ wordDict: [String]) -> Bool {
     
     return dfs(s)
 }
+
+//O(s.len^2 + wordDist.len), O(s.len + wordDist.len)
+class Solution {
+
+    class Trie {
+        var dict = [Character: Trie]()
+        var isEnd = false
+    }
+
+    func wordBreak(_ s: String, _ wordDict: [String]) -> Bool {
+        let root = Trie()
+        for word in wordDict {
+            var curr = root
+            for char in word {
+                if curr.dict[char] == nil {
+                    curr.dict[char] = Trie()
+                }
+                curr = curr.dict[char]!
+            }
+            curr.isEnd = true
+        }    
+
+        var memo = [[Character]: Bool]()
+        func dfs(_ arr: [Character]) -> Bool {
+            if arr.isEmpty {return true}
+            if memo[arr] != nil { return memo[arr]! }
+            var res = false
+            var curr = root
+            for i in 0..<arr.endIndex {
+                let char = arr[i]
+                if curr.dict[char] != nil {
+                    curr = curr.dict[char]!
+                    if curr.isEnd == true {
+                        res = res || dfs(Array(arr[(i+1)...]))
+                    }
+                    
+                } else {
+                    break
+                }
+            }
+            memo[arr] = res
+            return res
+        }
+        return dfs(Array(s))
+    }
+}
