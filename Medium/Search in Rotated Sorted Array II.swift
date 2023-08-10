e9ec109f9456a06b8e33bed3f04fa24d693b046f
Hsiang-Lin <zip520123@yahoo.com.tw>
2023-08-10 10:44:53 +0100

    Search in Rotated Sorted Array II

--- Medium/Search in Rotated Sorted Array II.swift
+++ Medium/Search in Rotated Sorted Array II.swift
@@ -26,3 +26,28 @@ func search(_ nums: [Int], _ target: Int) -> Bool {
     
     return false
 }
+// O(n)~o(log n), O(1)
+    func search(_ nums: [Int], _ target: Int) -> Bool {
+        var l = 0, r = nums.endIndex-1
+        while l<=r {
+            while l<r && nums[l] == nums[l+1] {l+=1} 
+            while l<r && nums[r] == nums[r-1] {r-=1} 
+            let mid = l+((r-l)>>1)
+            if nums[mid] == target {return true}
+            if nums[mid] >= nums[l] {
+                if target < nums[l] || target > nums[mid] {
+                    l = mid+1
+                } else {
+                    r = mid-1
+                }
+            } else {
+                if target > nums[r] || target < nums[mid] {
+                    r = mid-1
+                } else {
+                    l = mid+1
+                }
+            }
+        }
+
+        return false
+    }
