20250f850abac7967de94532edf564f0ce189bd2
Hsiang-Lin <zip520123@yahoo.com.tw>
2023-09-24 09:38:55 +0100

    Champagne Tower

--- Medium/Champagne Tower.swift
+++ Medium/Champagne Tower.swift
@@ -16,3 +16,19 @@
         
         return arr[query_row][query_glass] > 1 ? 1 : arr[query_row][query_glass]
     }
+
+// O(row^2), O(row^2)
+    func champagneTower(_ poured: Int, _ query_row: Int, _ query_glass: Int) -> Double {
+        var memo = [[Double]](repeating: [Double](repeating: 0, count: query_row+2), count: query_row+2)
+        memo[0][0] = Double(poured)
+        for row in 0...query_row {
+            for col in 0...row {
+                let q = (memo[row][col] - 1) / 2
+                if q > 0 {
+                    memo[row+1][col] += q
+                    memo[row+1][col+1] += q
+                }
+            }
+        }
+        return memo[query_row][query_glass] > 1 ? 1 : memo[query_row][query_glass]
+    }
