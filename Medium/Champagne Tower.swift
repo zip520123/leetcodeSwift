7a4c58b09e9997d8b2da90b4d1918f3db0581097
Hsiang-Lin <zip520123@yahoo.com.tw>
2023-09-24 09:47:52 +0100

    Champagne Tower

--- Medium/Champagne Tower.swift
+++ Medium/Champagne Tower.swift
@@ -19,9 +19,9 @@
 
 // O(row^2), O(row^2)
     func champagneTower(_ poured: Int, _ query_row: Int, _ query_glass: Int) -> Double {
-        var memo = [[Double]](repeating: [Double](repeating: 0, count: query_row+2), count: query_row+2)
+        var memo = [[Double]](repeating: [Double](repeating: 0, count: query_row+2), count: query_row+1)
         memo[0][0] = Double(poured)
-        for row in 0...query_row {
+        for row in 0..<query_row {
             for col in 0...row {
                 let q = (memo[row][col] - 1) / 2
                 if q > 0 {
