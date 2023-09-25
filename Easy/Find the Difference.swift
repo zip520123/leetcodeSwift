8740cb93ed0306212c83bb53fea8dfe21326318d
Hsiang-Lin <zip520123@yahoo.com.tw>
2023-09-25 22:41:24 +0900

    Find the Difference

--- Easy/Find the Difference.swift
+++ Easy/Find the Difference.swift
@@ -16,3 +16,20 @@
         }
         return Character("")
     }
+
+// O(s+t), O(s+t)
+    func findTheDifference(_ s: String, _ t: String) -> Character {
+        
+        let sArr = Array(s), tArr = Array(t)
+        var memo = [Character: Int]()
+        for char in sArr {
+            memo[char, default: 0] += 1
+        }
+        for char in tArr {
+            memo[char, default: 0] -= 1
+            if memo[char]! < 0 {
+                return char
+            }
+        }
+        return Character("")
+    }
