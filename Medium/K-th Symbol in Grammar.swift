09b457befa9968f1d1786ab1d410caa0f9ba4b05
Hsiang-Lin <zip520123@yahoo.com.tw>
2023-10-25 20:38:56 +0900

    K-th Symbol in Grammar

--- Medium/K-th Symbol in Grammar.swift
+++ Medium/K-th Symbol in Grammar.swift
@@ -1,17 +1,16 @@
 // K-th Symbol in Grammar
 // O(n), O(n)
 func kthGrammar(_ n: Int, _ k: Int) -> Int {
-    func dfs(_ n: Int, _ k: Int, _ rootVal: Int) -> Int {
-        if n == 1 {return rootVal}
+    func dfs(_ n: Int, _ k: Int, _ root: Int) -> Int {
+        if n == 1 {return root}
         let totalNodes = Int(pow(2, Double(n-1)))
         if k > totalNodes / 2 {
-            let nextRootVal = rootVal == 0 ? 1 : 0
-            return dfs(n-1, k-(totalNodes/2), nextRootVal)
+            let nextRoot = root == 0 ? 1 : 0
+            return dfs(n-1, k-totalNodes/2, nextRoot)
         } else {
-            let nextRootVal = rootVal == 0 ? 0 : 1
-            return dfs(n-1, k, nextRootVal)
+            let nextRoot = root == 0 ? 0 : 1
+            return dfs(n-1, k, nextRoot)
         }
     }
-
     return dfs(n,k,0)
 }
