668f901b2690ef02a0c2b539071d151aff5281fb
Hsiang-Lin <zip520123@yahoo.com.tw>
2023-11-11 20:58:18 +0900

    Design Graph With Shortest Path Calculator

--- misc/heap.swift
+++ misc/heap.swift
@@ -94,3 +94,51 @@ class Heap {
         return topElement
     }
 }
+
+class Heap<T: Comparable> {
+    private var arr = [T]()
+    var isEmpty: Bool { return arr.isEmpty }
+    func insert(_ n: T) {
+        arr.append(n)
+
+        var currIndex = arr.endIndex-1
+        while currIndex > 0 && arr[currIndex] < arr[(currIndex-1)/2] {
+            arr.swapAt((currIndex-1)/2, currIndex) 
+            currIndex = (currIndex-1)/2
+        }
+    }
+    
+    func top() -> T {
+        arr[0]
+    }
+    
+    func removeTop() -> T {
+        if arr.count == 1 {return arr.removeLast()}
+        let res = arr[0]
+        arr[0] = arr.removeLast()
+
+        // arr[0] = arr[arr.endIndex-1]
+        // arr.removeLast() 5x slower
+        
+        var currIndex = 0
+        
+        while true {
+            let leftIndex = currIndex*2+1
+            let rightIndex = currIndex*2+2
+            var targetIndex = currIndex
+
+            if leftIndex < arr.endIndex && arr[leftIndex] < arr[targetIndex] {
+                targetIndex = leftIndex
+            }
+            if rightIndex < arr.endIndex && arr[rightIndex] < arr[targetIndex] {
+                targetIndex = rightIndex
+            }
+            if targetIndex == currIndex {break}
+            arr.swapAt(targetIndex, currIndex) 
+            // (arr[currIndex], arr[targetIndex]) = (arr[targetIndex], arr[currIndex]) TLE
+            currIndex = targetIndex
+
+        }
+        return res
+    }
+}
