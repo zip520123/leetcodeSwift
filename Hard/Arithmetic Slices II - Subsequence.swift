0dcbc9c5324fc0705ead0b9a7d5950c2b666173b
zip520123 <zip520123@yahoo.com.tw>
2024-01-07 10:44:37 +0000

    Arithmetic Slices II - Subsequence

--- Hard/Arithmetic Slices II - Subsequence.swift
+++ Hard/Arithmetic Slices II - Subsequence.swift
@@ -1,32 +1,45 @@
 //Arithmetic Slices II - Subsequence
 //O(n^2), O(n^2)
-    func numberOfArithmeticSlices(_ nums: [Int]) -> Int {
-        var res = 0
-        var map = [[Int: Int]](repeating: [:], count: nums.count)
-        for i in 0..<nums.endIndex {
-            for j in 0..<i {
-                let diff = nums[j] - nums[i]
-                let c1 = map[i][diff, default:0]
-                let c2 = map[j][diff, default:0]
-                res += c2
-                map[i][diff,default:0] = c1+c2+1
-            }
+func numberOfArithmeticSlices(_ nums: [Int]) -> Int {
+    var res = 0
+    var map = [[Int: Int]](repeating: [:], count: nums.count)
+    for i in 0..<nums.endIndex {
+        for j in 0..<i {
+            let diff = nums[j] - nums[i]
+            let c1 = map[i][diff, default:0]
+            let c2 = map[j][diff, default:0]
+            res += c2
+            map[i][diff,default:0] = c1+c2+1
         }
-        return res
     }
+    return res
+}
 
-    //another solution
-    func numberOfArithmeticSlices(_ nums: [Int]) -> Int {
-        var res = 0
-        var map = [[Int:Int]](repeating: [:], count: nums.count)
-        for i in 0..<nums.endIndex {
-            for j in 0..<i {
-                let diff = nums[j] - nums[i]
-                let count = map[j][diff, default: 0]
+//another solution
+func numberOfArithmeticSlices(_ nums: [Int]) -> Int {
+    var res = 0
+    var map = [[Int:Int]](repeating: [:], count: nums.count)
+    for i in 0..<nums.endIndex {
+        for j in 0..<i {
+            let diff = nums[j] - nums[i]
+            let count = map[j][diff, default: 0]
 
-                res += count
-                map[i][diff, default: 0] += count + 1
-            }
+            res += count
+            map[i][diff, default: 0] += count + 1
         }
-        return res
     }
+    return res
+}
+
+func numberOfArithmeticSlices(_ nums: [Int]) -> Int {
+    var res = 0
+    var memo = [[Int: Int]](repeating: [:], count: nums.endIndex)
+    for i in 1..<nums.endIndex {
+        for j in 0..<i {
+            let diff = nums[i]-nums[j]
+            res += memo[j][diff, default: 0]
+            memo[i][diff, default: 0] += memo[j][diff, default: 0] + 1
+        }
+    }
+    return res
+}
