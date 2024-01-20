a75b4a16b8c29358ea41722c3c013b0c1e21f02a
zip520123 <zip520123@yahoo.com.tw>
2024-01-20 20:15:46 +0000

    Sum of Subarray Minimums

--- Medium/Sum of Subarray Minimums.swift
+++ Medium/Sum of Subarray Minimums.swift
@@ -18,3 +18,20 @@
         }
         return sums.reduce(0,+) % mod
     }
+
+// O(n), O(n)
+func sumSubarrayMins(_ arr: [Int]) -> Int {
+    let arr = [-Int.max] + arr + [-Int.max]
+    var stack = [Int]()
+    var res = 0
+    let mod = Int(1E9+7)
+    for i in 0..<arr.endIndex {
+        let curr = arr[i]
+        while !stack.isEmpty && arr[stack.last!] > curr {
+            let last = stack.removeLast()
+            res += arr[last] * (last-stack.last!) * (i-last) % mod
+        }
+        stack.append(i)
+    }
+    return res % mod
+}
