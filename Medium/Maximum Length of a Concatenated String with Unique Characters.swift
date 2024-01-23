550cb89f15ab335d53b777eed2ace93c2584d1d5
Hsiang-Lin <zip520123@yahoo.com.tw>
2024-01-23 14:56:04 +0000

    Maximum Length of a Concatenated String with Unique Characters

--- Medium/Maximum Length of a Concatenated String with Unique Characters.swift
+++ Medium/Maximum Length of a Concatenated String with Unique Characters.swift
@@ -13,3 +13,18 @@
         
         return dp.map { $0.count }.max()!
     }
+
+func maxLength(_ arr: [String]) -> Int {
+    var dp = [Set<Character>()]
+    for s in arr {
+        let curr_set = Set(s)
+        if curr_set.count < s.count {continue}
+        
+        for group in dp {
+            if group.isDisjoint(with: curr_set) {
+                dp.append(group.union(curr_set))
+            }
+        }
+    }
+    return dp.map { $0.count }.max()!
+}
