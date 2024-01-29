e6d35784c7937422e1cf48ea51344c90177fb2ae
zip520123 <zip520123@yahoo.com.tw>
2024-01-29 21:12:14 +0000

    Longest Palindromic Subsequence

--- Medium/Longest Palindromic Subsequence.swift
+++ Medium/Longest Palindromic Subsequence.swift
@@ -21,17 +21,35 @@
 //O(n^2), O(n^2)
     func longestPalindromeSubseq(_ s: String) -> Int {
         let arr = Array(s)
-        let n = arr.endIndex
-        var dp = [[Int]](repeating: [Int](repeating: 0, count: n), count: n)
-        for i in (0..<n).reversed() {
-            dp[i][i] = 1
-            for j in (i+1)..<n {
-                if arr[i] == arr[j] {
-                    dp[i][j] = dp[i+1][j-1]+2
+        var dp = [[Int]](repeating: [Int](repeating: 0, count: s.count + 1), count: s.count + 1)
+        let arr2 = Array(arr.reversed())
+        for i in 1...arr.endIndex {
+            for j in 1...arr.endIndex {
+                if arr[i-1] == arr2[j-1] {
+                    dp[i][j] = dp[i-1][j-1] + 1
                 } else {
-                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
                 }
             }
         }
-        return dp[0][n-1]
+        return dp[arr.endIndex][arr.endIndex]
     }
+
+/*
+ _ b b b a b
+_0 0 0 0 0 0
+b0 1 1 1 1 1
+a0 1 1 1 2 2
+b0 1 2 2 2 3
+b0 1 2 3 3 3
+b0 1 2 3 3 4
+
+ _ a a b c b a
+_0 0 0 0 0 0 0
+a0 1 1 1 1 1 1
+b0 1 1 2 2 2 2 
+c0 1 1 2 3 3 3
+b0 1 1 2 3 4 4
+a0 1 2 2 3 4 5
+a0 1 2 2 3 4 5
+*/
