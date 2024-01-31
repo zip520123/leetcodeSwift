fc1f3e829526193339b58c3b32d6f72d98e4c5d1
zip520123 <zip520123@yahoo.com.tw>
2024-01-31 21:30:33 +0000

    Daily Temperatures

--- Medium/Daily Temperatures.swift
+++ Medium/Daily Temperatures.swift
@@ -15,58 +15,18 @@
         }
         return res
     }
-//O(n), O(n)
-    func dailyTemperatures(_ temperatures: [Int]) -> [Int] {
-        var res = [Int](), stack = [Int]()
-        
-        for i in (0..<temperatures.endIndex).reversed() {
-            while stack.count > 0 && temperatures[stack.first!] <= temperatures[i] {
-                stack.removeFirst()
-            }
-            if stack.count == 0 {
-                res.insert(0, at: 0)
-            } else {
-                res.insert(stack.first!-i, at: 0)
-            }
-            stack.insert(i, at: 0)
-        }
-        
-        return res
-    }
 
-//O(n), O(n)
+// O(n), O(n)
     func dailyTemperatures(_ temperatures: [Int]) -> [Int] {
-        var res = (0..<temperatures.endIndex).map {_ in 0}
         var stack = [Int]()
-        
-        for i in (0..<temperatures.endIndex).reversed() {
-            while stack.count > 0 && temperatures[stack.first!] <= temperatures[i] {
-                stack.removeFirst()
-            }
-            if stack.count != 0 {
-                res[i] = stack.first!-i
-            }
-            stack.insert(i, at: 0)
-        }
-        
-        return res
-    }
-
-//O(n), O(n)
-    func dailyTemperatures(_ temperatures: [Int]) -> [Int] {
-        var res = [Int]()
-        var stack = [Int]()
-        for i in (0..<temperatures.endIndex).reversed() {
+        var res = temperatures.map { _ in 0 }
+        for i in 0..<temperatures.endIndex {
             let curr = temperatures[i]
-            while !stack.isEmpty && temperatures[stack.last!] <= curr {
+            while !stack.isEmpty && curr > temperatures[stack.last!] {
+                res[stack.last!] = i - stack.last!
                 stack.removeLast()
-            }
-            if stack.isEmpty {
-                res.append(0)
-            } else {
-                res.append(stack.last!-i)
             }
             stack.append(i)
         }
-        return res.reversed()
+        return res
     }
