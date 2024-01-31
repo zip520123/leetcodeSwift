69def6c1f1a94432bd3fcb864e79a11730c1e214
zip520123 <zip520123@yahoo.com.tw>
2024-01-31 21:55:06 +0000

    Valid Word Abbreviation

--- Easy/Valid Word Abbreviation.swift
+++ Easy/Valid Word Abbreviation.swift
@@ -1,22 +1,50 @@
 //Valid Word Abbreviation
 //O(w+a) O(w+a)
-    func validWordAbbreviation(_ word: String, _ abbr: String) -> Bool {
-        let wArr = Array(word), aArr = Array(abbr)
-        var i = 0, j = 0
-        while i < wArr.endIndex && j < aArr.endIndex {
-            if wArr[i] == aArr[j] {
-                i += 1; j += 1
-            } else if let n = Int(String(aArr[j])) {
-                if n == 0 {return false}
-                var num = 0
-                while j < aArr.endIndex, let n = Int(String(aArr[j])) {
-                    num = num*10 + n
-                    j += 1
+func validWordAbbreviation(_ word: String, _ abbr: String) -> Bool {
+    let wArr = Array(word), aArr = Array(abbr)
+    var i = 0, j = 0
+    while i < wArr.endIndex && j < aArr.endIndex {
+        if wArr[i] == aArr[j] {
+            i += 1; j += 1
+        } else if let n = Int(String(aArr[j])) {
+            if n == 0 {return false}
+            var num = 0
+            while j < aArr.endIndex, let n = Int(String(aArr[j])) {
+                num = num*10 + n
+                j += 1
+            }
+            i += num
+        } else {
+            return false
+        }
+    }
+    return i == word.count && j == abbr.count
+}
+
+// O(w+a), O(w+a)
+func validWordAbbreviation(_ word: String, _ abbr: String) -> Bool {
+    let a1 = Array(word), a2 = Array(abbr)
+    var l = 0, r = 0
+    while l < a1.endIndex && r < a2.endIndex {
+        if a1[l] == a2[r] {
+            l += 1
+            r += 1
+        } else {
+            if var currN = Int(String(a2[r])) {
+                if currN == 0 { return false }
+                while r+1<a2.endIndex, let nextN = Int(String(a2[r+1])) {
+                    currN *= 10
+                    currN += nextN
+                    r += 1
                 }
-                i += num
+                r += 1
+                for _ in 0..<currN {
+                    l += 1
+                }
             } else {
                 return false
             }
         }
-        return i == word.count && j == abbr.count
     }
+    return l == a1.endIndex && r == a2.endIndex
+}
