2ca497dfc9d454baf1528cf2796502f5ad3b91a5
Hsiang-Lin <zip520123@yahoo.com.tw>
2024-02-07 15:23:39 +0000

    Convert Binary Search Tree to Sorted Doubly Linked List

--- Medium/Convert Binary Search Tree to Sorted Doubly Linked List.swift
+++ Medium/Convert Binary Search Tree to Sorted Doubly Linked List.swift
@@ -22,45 +22,46 @@
     }
 //O(n) O(n)
     func treeToDoublyList(_ root: Node?) -> Node? {
-        var dummy = Node(0)
-        var prev = dummy
+        var head: Node?
+        var prev: Node?
         
         func dfs(_ node: Node?) {
-            guard let node = node else {return}
+            guard let node else {return}
             dfs(node.left)
-            prev.right = node
+            if head == nil { head = node }
             node.left = prev
+            prev?.right = node
             prev = node
             dfs(node.right)
         }
         
         dfs(root)
-        
-        dummy.right?.left = prev
-        prev.right = dummy.right
-        return dummy.right
+
+        head?.left = prev
+        prev?.right = head
+        return head
     }
 
 //O(n), O(n)
     func treeToDoublyList(_ root: Node?) -> Node? {
-        guard let node = root else {return nil}
-        var dummy = Node(0)
-        var prev: Node? = dummy
+        guard let root else {return nil}
         var stack = [Node]()
-        var curr = root
+        var curr: Node? = root
+        var prev: Node?
+        var head: Node?
         while !stack.isEmpty || curr != nil {
             while curr != nil {
                 stack.append(curr!)
                 curr = curr!.left
             }
-            let node = stack.removeLast()
-            prev?.right = node
-            node.left = prev
-            prev = node
-            curr = node.right
+            curr = stack.removeLast()
+            if head == nil { head = curr }
+            curr!.left = prev
+            prev?.right = curr
+            prev = curr
+            curr = curr!.right
         }
-        
-        dummy.right?.left = prev
-        prev?.right = dummy.right
-        return dummy.right
+        prev?.right = head
+        head?.left = prev
+        return head
     }
