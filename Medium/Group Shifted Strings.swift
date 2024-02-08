a8baa548e8536deaacae5ddcdbc1e8cd6e5c0f36
Hsiang-Lin <zip520123@yahoo.com.tw>
2024-02-08 16:54:55 +0000

    Group Shifted Strings

--- Medium/Group Shifted Strings.swift
+++ Medium/Group Shifted Strings.swift
@@ -15,3 +15,19 @@
         }
         return Array(dict.values)
     }
+
+// O(n), O(n)
+    func groupStrings(_ strings: [String]) -> [[String]] {
+        var memo = [[Int]:[String]]()
+        for s in strings {
+            var arr = [Int]()
+            let sArr = Array(s)
+            for i in 0..<sArr.endIndex-1 {
+                var diff = Int(sArr[i].asciiValue!) - Int(sArr[i+1].asciiValue!)
+                if diff < 0 {diff += 26}
+                arr.append(diff)
+            }
+            memo[arr, default: []].append(s)
+        }
+        return Array(memo.values)
+    }
