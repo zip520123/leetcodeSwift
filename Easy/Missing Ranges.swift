a23c26b7c8d9260f253ddc9276d6e26749d4312f
Hsiang-Lin <zip520123@yahoo.com.tw>
2024-02-13 11:27:14 +0000

    Missing Ranges

--- Easy/Missing Ranges.swift
+++ Easy/Missing Ranges.swift
@@ -53,3 +53,21 @@ class Solution {
         
         return res
     }
+
+    func findMissingRanges(_ nums: [Int], _ lower: Int, _ upper: Int) -> [[Int]] {
+        var res = [[Int]]()
+        if nums.endIndex == 0 { return [[lower, upper]] }
+        for i in 0..<nums.endIndex-1 {
+            let l = nums[i]+1, r = nums[i+1]-1
+            if l<=r {
+                res.append([l,r])
+            }
+        }
+        if lower < nums[0] {
+            res.insert([lower, nums[0]-1], at: 0)
+        }
+        if nums.last! < upper {
+            res.append([nums.last!+1, upper])
+        }
+        return res
+    }
