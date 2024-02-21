489542acb170a30067dba941cc8dc8b71c0e0827
zip520123 <zip520123@yahoo.com.tw>
2024-02-21 19:20:32 +0000

    Bitwise AND of Numbers Range

--- Medium/Bitwise AND of Numbers Range.swift
+++ Medium/Bitwise AND of Numbers Range.swift
@@ -1,5 +1,5 @@
 //Bitwise AND of Numbers Range
-//O(n), O(1)
+//O(1), O(1)
     func rangeBitwiseAnd(_ left: Int, _ right: Int) -> Int {
         var i = 0, l = left, r = right
         while l != r {
@@ -10,3 +10,16 @@
         return l << i
         
     }
+
+// O(1), O(1)
+func rangeBitwiseAnd(_ left: Int, _ right: Int) -> Int {
+    var res = 0
+    for i in 0..<32 {
+        if right-left < Int(pow(Double(2),Double(i))) && 
+        left & Int(pow(Double(2),Double(i))) != 0 && 
+        right & Int(pow(Double(2),Double(i))) != 0 {
+            res += Int(pow(Double(2),Double(i)))
+        }
+    }
+    return res
+}
