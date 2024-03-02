11eda8ccbab227f1860f471d563e5229cb29d887
zip520123 <zip520123@yahoo.com.tw>
2024-03-02 10:55:51 +0000

    Squares of a Sorted Array

--- Easy/Squares of a Sorted Array.swift
+++ Easy/Squares of a Sorted Array.swift
@@ -23,3 +23,20 @@ func sortedSquares(_ nums: [Int]) -> [Int] {
         
         return res
     }
+
+// O(n), O(n)
+func sortedSquares(_ nums: [Int]) -> [Int] {
+    var res = [Int]()
+    let arr = nums.map { $0 * $0 }
+    var l = 0, r = arr.endIndex-1
+    while l<=r {
+        if arr[l] < arr[r] {
+            res.append(arr[r])
+            r -= 1
+        } else {
+            res.append(arr[l])
+            l += 1
+        }
+    }
+    return res.reversed()
+}
