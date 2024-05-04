2fe4e33286dd10a463f48af5f0be01fa91b2f580
zip520123 <zip520123@yahoo.com.tw>
2024-05-04 09:52:44 +0100

    Boats to Save People

--- Medium/Boats to Save People.swift
+++ Medium/Boats to Save People.swift
@@ -1,15 +1,32 @@
 //Boats to Save People
 //O(n log n), O(n)
-    func numRescueBoats(_ people: [Int], _ limit: Int) -> Int {
-        let arr = people.sorted()
-        var i = 0, j = arr.endIndex-1
-        var res = 0
-        while i<=j {
-            res += 1
-            if arr[i] + arr[j] <= limit {
-                i += 1
-            }    
-            j -= 1
-        }
-        return res
+func numRescueBoats(_ people: [Int], _ limit: Int) -> Int {
+    let arr = people.sorted()
+    var i = 0, j = arr.endIndex-1
+    var res = 0
+    while i<=j {
+        res += 1
+        if arr[i] + arr[j] <= limit {
+            i += 1
+        }    
+        j -= 1
     }
+    return res
+}
+
+// O(n log n), O(n)
+func numRescueBoats(_ people: [Int], _ limit: Int) -> Int {
+    let arr = people.sorted()
+    var l = 0, r = arr.endIndex-1
+    var res = 0
+    while l<=r {
+        if arr[l] + arr[r] > limit {
+            r -= 1
+        } else {
+            l += 1
+            r -= 1
+        }
+        res += 1
+    }
+    return res
+}
